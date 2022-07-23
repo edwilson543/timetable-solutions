69d9b3d496037d18fccd3d9e0798265511631a6d
edwilson543 <edward.e.j.wilson@gmail.com>
2022-07-23 07:18:54 +0100

    updated models

--- timetable/timetable_selector/models.py
+++ timetable/timetable_selector/models.py
@@ -14,18 +14,29 @@ class Teacher(models.Model):
 
 class Pupil(models.Model):
     """Model for storing unique list of pupils."""
+
+    class YearGroup(models.IntegerChoices):
+        ONE = 1
+        TWO = 2
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
     firstname = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
+    year_group = models.IntegerField(choices=YearGroup.choices)
 
 
 class TimetableSlot(models.Model):
     """Model for stating the unique timetable slots when classes can take place"""
+
     class WeekDay(models.TextChoices):
         MONDAY = "MONDAY"
         TUESDAY = "TUESDAY"
         WEDNESDAY = "WEDNESDAY"
         THURSDAY = "THURSDAY"
         FRIDAY = "FRIDAY"
+
     class PeriodStart(dt.time, models.Choices):
         PERIOD_ONE = 9, 0, "PERIOD_ONE"
         PERIOD_TWO = 10, 0, "PERIOD_TWO"
@@ -45,7 +56,8 @@ class FixedClass(models.Model):
     Model for storing the unique list of classes that take place, when they take place, the teacher that takes the
     class, and the pupils that attend the class.
     """
+    class_id = models.CharField(max_length=20)  # Since may potentially want more complex ID than
     subject_name = models.CharField(max_length=20)
-    # time_slots = models. #  TODO later
     teacher = models.ForeignKey("Teacher", on_delete=models.PROTECT, related_name="teacher")
     pupils = models.ManyToManyField("Pupil", related_name="pupils")
+    time_slots = models.ManyToManyField("TimetableSlot", related_name="slots")
