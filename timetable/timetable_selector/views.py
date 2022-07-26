7aaae3fe931540e629f0f6410006211516edc1ea
edwilson543 <edward.e.j.wilson@gmail.com>
2022-07-26 08:36:19 +0100

    Added view and template for seeing list of pupils. Not yet linked through to anything.

--- timetable/timetable_selector/views.py
+++ timetable/timetable_selector/views.py
@@ -2,6 +2,9 @@
 from django.http import HttpResponse
 from django.template import loader
 
+# Local application imports
+from .models import Pupil
+
 
 def selection_navigator(request):
     """View to bring up the main navigation screen for guiding the user towards individual timetables."""
@@ -13,7 +16,17 @@ def teacher_navigator(request):
     """View to bring up a list of teachers which can be linked out to each of their timetables."""
     pass
 
+
 def pupil_navigator(request):
-    """View to bring up a list of pupils which can be linked out to each of their timetables."""
+    """
+    View to provide a dictionary of pupils which can be linked out to each of their timetables.
+    This is pre-processed to be indexed by year group for display in the template.
+    """
+    year_indexed_pupils = {year: Pupil.objects.filter(year_group=year).order_by("surname").values() for
+                           year in Pupil.YearGroup.values}
+    year_indexed_pupils = {key: value for key, value in year_indexed_pupils.items() if len(value) > 0}
     template = loader.get_template("pupils_navigator.html")
-    return HttpResponse(template.render({}, request))
+    context = {
+        "all_pupils": year_indexed_pupils
+    }
+    return HttpResponse(template.render(context, request))
