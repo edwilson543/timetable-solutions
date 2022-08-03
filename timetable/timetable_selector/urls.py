952cb2199cb82cde7cd4f33151ff7d9a2c2efde0
edwilson543 <edward.e.j.wilson@gmail.com>
2022-08-03 08:04:36 +0100

    Added prototype teacher timetable template, however colour scheme needs fixing

--- timetable/timetable_selector/urls.py
+++ timetable/timetable_selector/urls.py
@@ -9,6 +9,7 @@ from . import views
 urlpatterns = [
     path('', views.selection_navigator, name='selection_navigator'),
     path('teachers/', views.teacher_navigator, name='teachers_navigator'),
+    path('teachers/<int:id>', views.teacher_timetable_view, name='teacher_timetable_view'),
     path('pupils/', views.pupil_navigator, name='pupils_navigator'),
     path('pupils/<int:id>', views.pupil_timetable_view, name='pupil_timetable_view')
 ]
