7dc30c71d0298da6fbaad3d69d50f191c4104bd3
edwilson543 <edward.e.j.wilson@gmail.com>
2022-08-03 06:43:26 +0100

    Split out function for getting a time/day indexed timetable into its own function so that it can be reused for both teachers and pupils

--- timetable/timetable_selector/views.py
+++ timetable/timetable_selector/views.py
@@ -1,4 +1,8 @@
+# Standard library imports
+from typing import Dict, List, Tuple
+
 # Django imports
+from django.db.models import QuerySet
 from django.http import HttpResponse
 from django.template import loader
 
@@ -48,32 +52,20 @@ def teacher_navigator(request) -> HttpResponse:
 def pupil_timetable_view(request, id: int) -> HttpResponse:
     """
     View for the timetable of the individual pupil with the passed id.
-    The main context is 'timetable' - a nested dictionary where the outermost key is the time/period (9am/10am/...), the
-    innermost key is the day of the week, and the values are the subject objects at each relevant timeslot, with the
-    exception that a free period is just a string 'FREE'.
-    e.g. {9AM: {MONDAY: MATHS, TUESDAY: FRENCH,...}, 10AM: {...}, ...}
-    This structure is chosen such that it can be efficiently iterated over in the template to create a html table.
+    Context:
+    ----------
+    timetable - see _get_timetable_slot_indexed_timetable
+    pupil - an instance of the Pupil model
+    class_colours - a dictionary
     """
     # noinspection PyUnresolvedReferences
     pupil = Pupil.objects.get(id=id)
     classes = pupil.classes.all()
-    class_indexed_timetable = {klass: klass.time_slots.all().values() for klass in classes}
-    timetable = {}
-    for time in TimetableSlot.PeriodStart.values:
-        time_timetable = {}  # specific times as indexes to nested dicts, indexed by days: {9AM: {Monday: [...]}...}
-        for day in TimetableSlot.WeekDay.values:
-            # noinspection PyUnresolvedReferences
-            for klass, time_slots in class_indexed_timetable.items():
-                queryset = time_slots.filter(day_of_week=day, period_start_time=time)
-                if queryset.exists():
-                    time_timetable[day] = klass
-            if day not in time_timetable:
-                time_timetable[day] = FixedClass.SubjectColour.FREE.name
-        timetable[time] = time_timetable
+    timetable = _get_timetable_slot_indexed_timetable(classes=classes)
 
     class_colours = {klass.subject_name: FixedClass.SubjectColour.get_colour_from_subject(
-        subject_name=klass.subject_name) for klass in class_indexed_timetable}
-    class_colours = class_colours | {"FREE": FixedClass.SubjectColour.get_colour_from_subject(
+        subject_name=klass.subject_name) for klass in classes}
+    class_colours = class_colours | {f"FREE": FixedClass.SubjectColour.get_colour_from_subject(
             FixedClass.SubjectColour.FREE.name)}
 
     template = loader.get_template("pupil_timetable.html")
@@ -93,3 +85,31 @@ def teacher_timetable_view(request, id: int) -> HttpResponse:
     # TODO first split out the reusable part of the pupil timetable view.
     # For the template, create a css file for the timetable.
     pass
+
+
+def _get_timetable_slot_indexed_timetable(classes: QuerySet | List[FixedClass]) -> \
+        Dict[Tuple[TimetableSlot.PeriodStart, TimetableSlot.WeekDay] | str, QuerySet | List[FixedClass]]:
+    """
+    Function to return a timetable data structure that can easily be iterated over in a django template.
+
+    Parameters: classes - this is a filtered QuerySet from the FixedClass model, for exactly 1 teacher/pupil
+    Returns: timetable - a nested dictionary where the outermost key is the time/period (9am/10am/...), the
+    innermost key is the day of the week, and the values are the subject objects at each relevant timeslot, with the
+    exception that a free period is just a string 'FREE'.
+    e.g. {9AM: {MONDAY: MATHS, TUESDAY: FRENCH,...}, 10AM: {...}, ...}
+    This structure is chosen such that it can be efficiently iterated over in the template to create a html table.
+    """
+    class_indexed_timetable = {klass: klass.time_slots.all().values() for klass in classes}
+    timetable = {}
+    for time in TimetableSlot.PeriodStart.values:
+        time_timetable = {}  # specific times as indexes to nested dicts, indexed by days: {9AM: {Monday: [...]}...}
+        for day in TimetableSlot.WeekDay.values:
+            # noinspection PyUnresolvedReferences
+            for klass, time_slots in class_indexed_timetable.items():
+                queryset = time_slots.filter(day_of_week=day, period_start_time=time)
+                if queryset.exists():
+                    time_timetable[day] = klass
+            if day not in time_timetable:
+                time_timetable[day] = FixedClass.SubjectColour.FREE.name
+        timetable[time] = time_timetable
+    return timetable
