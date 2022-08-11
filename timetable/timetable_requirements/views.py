1520f82a04264e9c7b25773799d1e0c2998ba443
edwilson543 <edward.e.j.wilson@gmail.com>
2022-08-11 08:48:44 +0100

    Started writing template which will contain all the file upload fields

--- timetable/timetable_requirements/views.py
+++ timetable/timetable_requirements/views.py
@@ -1,5 +1,10 @@
 
+# Standard library imports
+from dataclasses import dataclass
+from typing import Dict
+
 # Django imports
+from django.forms import Form
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
@@ -11,19 +16,39 @@ from .file_upload_processor import FileUploadProcessor
 from timetable_selector.models import Teacher
 
 
-def get_all_forms():
-    """This will be updated to be from an upload status model."""
-    return {"teacher_form": TeacherListUploadForm()}
+@dataclass
+class RequiredUpload:
+    """Dataclass to store information relating to each form"""
+    form_name: str
+    upload_status: str | bool
+    empty_form: Form
+
+    def __post_init__(self):
+        """Reset upload status to a string which is more easily rendered in the template, without unnecessary tags."""
+        if self.upload_status:
+            self.upload_status = "Complete"
+        else:
+            self.upload_status = "Incomplete"
+
+
+def _get_all_form_context() -> Dict:
+    """Function to get a dictionary of forms that must be populated (note each form has just one file field
+    to allow files to be uploaded separately)."""
+    teacher_upload_status = len(Teacher.objects.all()) > 0
+    context = {"teachers": RequiredUpload(form_name="Teacher list", upload_status=teacher_upload_status,
+                                          empty_form=TeacherListUploadForm())}
+    return context
 
 
 def upload_page_view(request):
     """Initial view for loading the upload page."""
     template = loader.get_template("file_upload.html")
-    context = get_all_forms()
+    context = _get_all_form_context()
     return HttpResponse(template.render(context, request))
 
 
 class TeacherListUploadView(View):
+    """View to control upload of teacher list to database"""
     template_name = "file_upload.html"
     csv_headers = ["teacher_id", "firstname", "surname", "title"]
     id_column_name = "teacher_id"
@@ -32,14 +57,10 @@ class TeacherListUploadView(View):
     def post(self, request, *args, **kwargs):
         """Method for handling a POST request"""
         form = TeacherListUploadForm(request.POST, request.FILES)
-        context = {
-            "success": False
-        }
         if form.is_valid():
             file = request.FILES["teacher_list"]
             upload_processor = FileUploadProcessor(
                 csv_file=file, csv_headers=self.csv_headers, id_column_name=self.id_column_name, model=self.model)
-            if upload_processor.upload_successful:
-                context["success"] = True
+        context = _get_all_form_context()
         return render(request, self.template_name, context)
 
