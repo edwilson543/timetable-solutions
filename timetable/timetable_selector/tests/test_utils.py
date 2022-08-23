dbd56a5175c81bc5350ae4c16bcdc67510a404ba
edwilson543 <edward.e.j.wilson@gmail.com>
2022-08-23 07:25:42 +0100

    Wrote function for getting some brief summary stats from the database. Added test for this and working as expected

---
+++ timetable/timetable_selector/tests/test_utils.py
@@ -0,0 +1,24 @@
+"""Module containing unit tests for the utility functions in timetable_selector app."""
+
+# Django imports
+from django.test import TestCase
+
+# Local application imports
+from ..utils import get_summary_stats
+
+
+class TestViews(TestCase):
+    """Test class for function in the utils.py module of timetable_selector views"""
+    fixtures = ["users.json", "classrooms.json", "pupils.json", "teachers.json", "timetable.json", "classes.json"]
+
+    def test_get_summary_stats_correct(self):
+        """
+        Test that get summary stats provides the correct values for summarising the (known) contents
+        of the test fixtures.
+        """
+        stats = get_summary_stats(school_access_key=123456)
+        # All assertion values are evident from the tests fixtures
+        self.assertEqual(stats["total_classes"], 12)
+        self.assertEqual(stats["total_lessons"], 100)
+        self.assertEqual(stats["total_pupils"], 6)
+        self.assertEqual(stats["total_teachers"], 11)
--- timetable/timetable_selector/utils.py
+++ timetable/timetable_selector/utils.py
@@ -7,11 +7,12 @@ from typing import Dict, List
 from django.db.models import QuerySet, Q
 
 # Local application imports
-from .models import FixedClass, TimetableSlot
+from .models import FixedClass, TimetableSlot, Pupil, Teacher
 from users.models import School
 
 
-def get_timetable_solution_summary(school_access_key: int) -> Dict:
+# noinspection PyUnresolvedReferences
+def get_summary_stats(school_access_key: int) -> Dict:
     """
     Function to extract some summary statistics on the timetable solutions that have been found, to be displayed on
     the selection_dashboard
@@ -19,18 +20,23 @@ def get_timetable_solution_summary(school_access_key: int) -> Dict:
     school = School.objects.get(school_access_key=school_access_key)
 
     # Get the querysets used to create summary statistics
-    all_classes = FixedClass.objects.filter(Q(school=school) & ~Q(subject_name="LUNCH"))
+    all_classes = FixedClass.objects.filter(Q(school=school) & ~Q(subject_name__contains="LUNCH"))
 
-    all_slots = TimetableSlot.objects.filter(Q(school))
+    all_slots = TimetableSlot.objects.filter(school=school)
     all_slot_classes = {(slot.period_start_time, slot.day_of_week): slot.classes for slot in all_slots}
-    slot_class_count = {key: len([klass for klass in klasses.all() if klass.subject_name != "LUNCH"]) for
+    slot_class_count = {key: len([klass for klass in klasses.all() if "LUNCH" not in klass.subject_name]) for
                         key, klasses in all_slot_classes.items()}
+    pupils = Pupil.objects.all()
+    teachers = Teacher.objects.all()
 
     stats = {
         "total_classes": len(all_classes),
         "total_lessons": sum(slot_class_count.values()),
         "busiest_slot": max(slot_class_count, key=slot_class_count.get),
+        "total_pupils": len(pupils),
+        "total_teachers": len(teachers),
     }
+    return stats
 
 
 def get_timetable_slot_indexed_timetable(classes: QuerySet | List[FixedClass]) -> Dict:
