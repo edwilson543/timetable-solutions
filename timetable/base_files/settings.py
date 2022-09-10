26d76bd0880a9618f4e6ecca1cbae00081698d37
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 09:10:55 +0100

    Renamed location of the base django files (settings etc.) to become 'base_files'

--- timetable/base_files/settings.py
+++ timetable/base_files/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   BASE_DIR / 'timetable' / 'static',
+   BASE_DIR / 'base_files' / 'static',
    ]
 
 # Default primary key field type
