501eb8bd7a8ce82d8f10062ddaabff234e9408af
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 09:24:58 +0100

    Renamed location of the base django files (settings etc.) to become 'base_files'

--- timetable/base_files/asgi.py
+++ timetable/base_files/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timetable.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base_files.settings')
 
 application = get_asgi_application()
