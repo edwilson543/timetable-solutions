fb491f49edbd21fb5631834b10f93251336df600
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 08:52:41 +0100

    Fixed convenience imports to use correct relative filepaths

--- timetable/data/models/__init__.py
+++ timetable/data/models/__init__.py
@@ -1,9 +1,9 @@
 # Convenience imports of all models
-from user_profile import Profile
-from school import School
-from pupil import Pupil
-from teacher import Teacher
-from classroom import Classroom
-from timetable_slot import TimetableSlot
-from fixed_class import FixedClass
-from unsolved_class import UnsolvedClass
+from .user_profile import Profile
+from .school import School
+from .pupil import Pupil
+from .teacher import Teacher
+from .classroom import Classroom
+from .timetable_slot import TimetableSlot
+from .fixed_class import FixedClass
+from .unsolved_class import UnsolvedClass
