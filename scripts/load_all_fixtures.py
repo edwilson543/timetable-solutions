ba79b5a675f35e54810ac304c92e1640ce1c7f71
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 17:19:20 +0100

    Deleted the data migration and moved it to scripts - having a data migration messes up the tests which load the same fixtures, since not all tests use all fixtures that the data migration was loading

--- scripts/load_all_fixtures.py
+++ scripts/load_all_fixtures.py
@@ -1,13 +1,14 @@
 """
-Data migration to automatically include all data fixtures into the database so that there is something to view on the sit
+Script to load all fixtures into the database in the correct order, so that there is something to view on the site.
+A data migration is not used, to automatically include all data fixtures into the database, because this is not wanted
+for the tests.
 """
 
 # Django imports
 from django.core.management import call_command
-from django.db import migrations
 
 
-def load_initial_data(*args, **kwargs):
+def load_initial_data():
     """Function to load in the initial data from the JSON fixtures, in the correct order"""
     call_command("loaddata", "user_school_profile.json")  # All other models depend on this fixture
     call_command("loaddata", "classrooms.json")
@@ -17,12 +18,5 @@ def load_initial_data(*args, **kwargs):
     call_command("loaddata", "fixed_classes.json")  # This fixture depends on the installation of all other fixtures
 
 
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('data', '0001_initial'),
-    ]
-
-    operations = [
-        migrations.RunPython(load_initial_data)
-    ]
+if __name__ == "__main__":
+    load_initial_data()
