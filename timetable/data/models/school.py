3e64418d99e8acc071f5bee644a37167c33e1a3f
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 16:01:08 +0100

    Introduced a custom query set manager for the School model

--- timetable/data/models/school.py
+++ timetable/data/models/school.py
@@ -4,12 +4,22 @@
 from django.db import models
 
 
+class SchoolQuerySet(models.QuerySet):
+    """Custom queryset manager for the School model"""
+    def get_individual_school(self, school_id):
+        """Method returning a specific School instance"""
+        return self.get(school_access_key=school_id)
+
+
 class School(models.Model):
     """
     Model representing a school_id, with every other model associated with one school_id instance via a foreign key
     """
     school_access_key = models.SmallIntegerField(primary_key=True)
     school_name = models.CharField(max_length=50)
+
+    # Introduce a custom manager
+    objects = SchoolQuerySet.as_manager()
 
     def __str__(self):
         """String representation of the model for the django admin site"""
@@ -21,3 +31,40 @@ class School(models.Model):
         """Method to create a new School instance."""
         school = cls.objects.create(school_access_key=school_access_key, school_name=school_name)
         return school
+
+    # PROPERTIES
+    @property
+    def has_teacher_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded Teacher data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.teacher_set.exists()
+
+    @property
+    def has_pupil_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded Pupil data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.pupil_set.exists()
+
+    @property
+    def has_classroom_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded Classroom data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.classroom_set.exists()
+
+    @property
+    def has_timetable_structure_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded TimetableSlot data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.timetableslot_set.exists()
+
+    @property
+    def has_unsolved_class_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded UnsolvedClass data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.unsolvedclass_set.exists()
+
+    @property
+    def has_fixed_class_data(self) -> bool:
+        """Indicates whether a user from the given school has uploaded FixedClass data into the database."""
+        # noinspection PyUnresolvedReferences
+        return self.fixedclass_set.exists()
