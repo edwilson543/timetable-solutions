6e8ffe0f0f767a2e42a07b2bc976c2ce9d4cd4eb
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-10 08:39:53 +0100

    Registered models to the admin from the data app (rather than within each individual django app)

--- timetable/timetable_requirements/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import UnsolvedClass
-
-admin.site.register(UnsolvedClass)
--- timetable/timetable_selector/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Pupil, Teacher, Classroom, FixedClass, TimetableSlot
-
-admin.site.register(Pupil)
-admin.site.register(Teacher)
-admin.site.register(Classroom)
-admin.site.register(FixedClass)
-admin.site.register(TimetableSlot)
