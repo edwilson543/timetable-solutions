4176186b5f74ec0c9e1b58306bb7d0c746d4d21f
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-11 09:01:38 +0100

    Renamed class_requirements to unsolved_classes, in line with naming elsewhere in the repository

--- timetable/domain/data_upload_processing/constants.py
+++ timetable/domain/data_upload_processing/constants.py
@@ -49,9 +49,9 @@ class UploadFileStructure:
                                id_column=Header.CLASSROOM_ID)
     TIMETABLE = FileStructure(headers=[Header.SLOT_ID, Header.DAY_OF_WEEK, Header.PERIOD_STARTS_AT, Header.PERIOD_DURATION],
                               id_column=Header.SLOT_ID)
-    CLASS_REQUIREMENTS = FileStructure(headers=[Header.CLASS_ID, Header.SUBJECT_NAME, Header.TEACHER_ID, Header.PUPIL_IDS,
-                                                Header.CLASSROOM_ID, Header.TOTAL_SLOTS, Header.MIN_DISTINCT_SLOTS],
-                                       id_column=Header.CLASS_ID)
+    UNSOLVED_CLASSES = FileStructure(headers=[Header.CLASS_ID, Header.SUBJECT_NAME, Header.TEACHER_ID, Header.PUPIL_IDS,
+                                              Header.CLASSROOM_ID, Header.TOTAL_SLOTS, Header.MIN_DISTINCT_SLOTS],
+                                     id_column=Header.CLASS_ID)
     FIXED_CLASSES = FileStructure(headers=[Header.CLASS_ID, Header.SUBJECT_NAME, Header.TEACHER_ID, Header.PUPIL_IDS,
                                            Header.CLASSROOM_ID, Header.SLOT_IDS],
                                   id_column=Header.CLASS_ID)
