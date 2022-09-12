207c92b6836b81348abf0653fb204f3b067f7a0d
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-12 07:38:58 +0100

    Added unit test for successful upload of each csv file direct from the domain

--- timetable/tests/unit/domain/test_file_upload_processor.py
+++ timetable/tests/unit/domain/test_file_upload_processor.py
@@ -1,5 +1,8 @@
 """Module containing unit tests for the FileUploadProcessor"""
 
+# Standard library imports
+import datetime as dt
+
 # Django imports
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
@@ -10,13 +13,15 @@ from data import models
 from domain import data_upload_processing
 
 
-class TestFileUploadProcessor(TestCase):
+class TestFileUploadProcessorIndependentFiles(TestCase):
+    """Tests for the file uploads that depend on no existing data in the database"""
 
     fixtures = ["user_school_profile.json"]
     test_data_folder = BASE_DIR / "tests" / "test_data"
 
     def test_upload_teachers_to_database_valid_upload(self):
-        """Unit test that the FileUploadProcessor is able to take a csv file and save it to the database."""
+        """Test that the FileUploadProcessor can upload the teacher csv file and use it to populate database"""
+        # Upload the file
         with open(self.test_data_folder / "teachers.csv", "rb") as csv_file:
             upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
             upload_processor = data_upload_processing.FileUploadProcessor(
@@ -24,13 +29,127 @@ class TestFileUploadProcessor(TestCase):
                 id_column_name=data_upload_processing.UploadFileStructure.TEACHERS.id_column,
                 model=models.Teacher, school_access_key=123456)
 
-        # Test that attribute 'upload_successful' has been set to True, indicating that everything has worked
+        # Test the upload was successful
         self.assertTrue(upload_processor.upload_successful)
 
-        # Test that the database is as expected
+        # Test the database is as expected
         all_teachers = models.Teacher.objects.get_all_school_teachers(school_id=123456)
         self.assertEqual(len(all_teachers), 11)
         greg = models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=6)
         self.assertIsInstance(greg, models.Teacher)
         self.assertEqual(greg.firstname, "Greg")
         self.assertEqual(greg.surname, "Thebaker")
+
+    def test_upload_pupils_to_database_valid_upload(self):
+        """Test that the FileUploadProcessor can upload the pupil csv file and use it to populate database"""
+        # Upload the file
+        with open(self.test_data_folder / "pupils.csv", "rb") as csv_file:
+            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
+            upload_processor = data_upload_processing.FileUploadProcessor(
+                csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.PUPILS.headers,
+                id_column_name=data_upload_processing.UploadFileStructure.PUPILS.id_column,
+                model=models.Pupil, school_access_key=123456)
+
+        # Test the upload was successful
+        self.assertTrue(upload_processor.upload_successful)
+
+        # Test that the database is as expected
+        all_pupils = models.Pupil.objects.get_all_school_pupils(school_id=123456)
+        self.assertEqual(len(all_pupils), 6)
+        teemu = models.Pupil.objects.get_individual_pupil(school_id=123456, pupil_id=5)
+        self.assertEqual(teemu.firstname, "Teemu")
+        self.assertEqual(teemu.surname, "Pukki")
+
+    def test_upload_classrooms_to_database_valid_upload(self):
+        """Test that the FileUploadProcessor can upload the classroom csv file and use it to populate database"""
+        # Upload the file
+        with open(self.test_data_folder / "classrooms.csv", "rb") as csv_file:
+            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
+            upload_processor = data_upload_processing.FileUploadProcessor(
+                csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.CLASSROOMS.headers,
+                id_column_name=data_upload_processing.UploadFileStructure.CLASSROOMS.id_column,
+                model=models.Classroom, school_access_key=123456)
+
+        # Test the upload was successful
+        self.assertTrue(upload_processor.upload_successful)
+
+        # Test that the database is as expected
+        all_classrooms = models.Classroom.objects.get_all_school_classrooms(school_id=123456)
+        self.assertEqual(len(all_classrooms), 12)
+        room = models.Classroom.objects.get_individual_classroom(school_id=123456, classroom_id=11)
+        self.assertEqual(room.room_number, 40)
+
+    def test_upload_timetable_structure_to_database_valid_upload(self):
+        """Test that the FileUploadProcessor can upload the timetable csv file and use it to populate database"""
+        # Upload the file
+        with open(self.test_data_folder / "timetable.csv", "rb") as csv_file:
+            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
+            upload_processor = data_upload_processing.FileUploadProcessor(
+                csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.TIMETABLE.headers,
+                id_column_name=data_upload_processing.UploadFileStructure.TIMETABLE.id_column,
+                model=models.TimetableSlot, school_access_key=123456)
+
+        # Test the upload was successful
+        self.assertTrue(upload_processor.upload_successful)
+
+        # Test that the database is as expected
+        all_slots = models.TimetableSlot.objects.get_all_school_timeslots(school_id=123456)
+        self.assertEqual(len(all_slots), 35)
+        slot = models.TimetableSlot.objects.get_individual_timeslot(school_id=123456, slot_id=1)
+        self.assertEqual(slot.day_of_week, "MONDAY")
+        self.assertEqual(slot.period_starts_at, dt.time(hour=9))
+        self.assertEqual(slot.period_duration, dt.timedelta(hours=1))
+
+
+class TestFileUploadProcessorDependentFiles(TestCase):
+    """Tests for the file uploads that depend on no existing data in the database, hence requiring fixtures."""
+
+    fixtures = ["user_school_profile.json", "classrooms.json", "pupils.json", "teachers.json", "timetable.json"]
+    test_data_folder = BASE_DIR / "tests" / "test_data"
+
+    def test_upload_unsolved_classes_to_database_valid_upload(self):
+        """
+        Test that the FileUploadProcessor can upload the class requirements (unsolved classes) csv file and use it
+        to populate database
+        """
+        # Upload the file
+        with open(self.test_data_folder / "class_requirements.csv", "rb") as csv_file:
+            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
+            upload_processor = data_upload_processing.FileUploadProcessor(
+                csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.UNSOLVED_CLASSES.headers,
+                id_column_name=data_upload_processing.UploadFileStructure.UNSOLVED_CLASSES.id_column,
+                model=models.UnsolvedClass, school_access_key=123456, is_unsolved_class_upload=True)
+
+        # Test the upload was successful
+        self.assertTrue(upload_processor.upload_successful)
+
+        # Test the database is as expected
+        all_classes = models.UnsolvedClass.objects.get_all_school_unsolved_classes(school_id=123456)
+        assert len(all_classes) == 12
+        klass = models.UnsolvedClass.objects.get_individual_unsolved_class(school_id=123456,
+                                                                           class_id="YEAR_ONE_MATHS_A")
+        self.assertQuerysetEqual(klass.pupils.all(), models.Pupil.objects.filter(pupil_id__in={1, 2}), ordered=False)
+        self.assertEqual(klass.teacher, models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=1))
+
+    def test_upload_fixed_classes_to_database_valid_upload(self):
+        """Test that the FileUploadProcessor can upload the fixed classes csv file and use it to populate database"""
+        # Upload the file
+        with open(self.test_data_folder / "fixed_classes.csv", "rb") as csv_file:
+            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
+            upload_processor = data_upload_processing.FileUploadProcessor(
+                csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.FIXED_CLASSES.headers,
+                id_column_name=data_upload_processing.UploadFileStructure.FIXED_CLASSES.id_column,
+                model=models.FixedClass, school_access_key=123456, is_fixed_class_upload=True)
+
+        # Test the upload was successful
+        self.assertTrue(upload_processor.upload_successful)
+
+        # Test the database is as expected
+        all_classes = models.FixedClass.objects.get_all_school_fixed_classes(school_id=123456)
+        assert len(all_classes) == 12
+        pup_lunch = models.FixedClass.objects.get_individual_fixed_class(school_id=123456, class_id="LUNCH_PUPILS")
+        self.assertQuerysetEqual(pup_lunch.pupils.all(),
+                                 models.Pupil.objects.get_all_school_pupils(school_id=123456), ordered=False)
+        teach_ten_lunch = models.FixedClass.objects.get_individual_fixed_class(school_id=123456, class_id="LUNCH_10")
+        self.assertEqual(teach_ten_lunch.teacher,
+                         models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=10))
