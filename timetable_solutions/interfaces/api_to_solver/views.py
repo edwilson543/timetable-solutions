6a3aa568c306cd13514e56aabaaaaf5cd772b4d0
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-19 10:08:22 +0100

    Added filtering of the FixedClass query set by school access key

--- timetable_solutions/interfaces/api_to_solver/views.py
+++ timetable_solutions/interfaces/api_to_solver/views.py
@@ -1,5 +1,3 @@
-
-
 # Third party imports
 from rest_framework import viewsets
 
@@ -11,4 +9,21 @@ from interfaces.api_to_solver import serialisers
 class FixedClassViewSet(viewsets.ModelViewSet):
     """Viewset for the FixedClass model"""
     serializer_class = serialisers.FixedClassSerialiser
-    queryset = models.FixedClass.objects.all()  # TODO replace with method on the queryset manager
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned serialised FixedClass instances, by checking the url for filtering against a
+        specific school.
+        e.g. domain/api/fixedclasses?school_access_key=123456
+        """
+        school_access_key = self.request.query_params.get("school_access_key")
+        if school_access_key is not None:
+            try:
+                school_access_key = int(school_access_key)
+                fixed_classes = models.FixedClass.objects.get_all_school_fixed_classes(school_id=school_access_key)
+                return fixed_classes
+            except ValueError:  # URL query specified a school access key but it was not an integer
+                # TODO in this instance ensure response status code is 204
+                return None
+        else:
+            return None
