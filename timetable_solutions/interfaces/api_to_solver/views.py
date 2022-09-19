15e91e73ab6f771c7863eda6881172cc6b39e1e4
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-19 09:11:48 +0100

    Added a ModelViewSet for the FixedClass model

--- timetable_solutions/interfaces/api_to_solver/views.py
+++ timetable_solutions/interfaces/api_to_solver/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+# Third party imports
+from rest_framework import viewsets
+
+# Local application imports
+from data import models
+from interfaces.api_to_solver import serialisers
+
+
+class FixedClassViewSet(viewsets.ModelViewSet):
+    """Viewset for the FixedClass model"""
+    serializer_class = serialisers.FixedClassSerialiser
+    queryset = models.FixedClass.objects.all()  # TODO replace with method on the queryset manager
