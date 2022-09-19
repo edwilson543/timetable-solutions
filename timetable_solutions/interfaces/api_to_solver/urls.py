d2a64b31dcccaa854cc7e49a55a18e416cbcd51c
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-19 09:23:39 +0100

    Added url routing for the fixedclass ViewSet

---
+++ timetable_solutions/interfaces/api_to_solver/urls.py
@@ -0,0 +1,16 @@
+# Django imports
+from django import urls
+
+# Third party imports
+from rest_framework import routers
+
+# Local application imports
+from interfaces.api_to_solver import views
+
+
+router = routers.DefaultRouter()
+router.register(r"fixedclasses", viewset=views.FixedClassViewSet, basename="fixedclasses")
+
+url_patterns = [
+    urls.path("", urls.include(router.urls))
+]
