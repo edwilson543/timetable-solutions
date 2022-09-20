9c0b3821235db9ef0d4d091a79f7327d4dcaabb7
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-20 19:35:01 +0100

    Added serializers for the UnsolvedClass and UnsolvedTimetableSlot

--- timetable_solutions/interfaces/api_to_solver/serialisers.py
+++ timetable_solutions/interfaces/api_to_solver/serialisers.py
@@ -1,3 +1,9 @@
+"""
+Django-rest framework serializer classes for the models that are required by the solver.
+The solver only needs to be aware of FixedClass, UnsolvedClass, and TimetableSlot - all other models e.g. pupils are
+identifiable from the model field relationships, and suffice to be represented by primary keys only within the solver.
+"""
+
 # Third party imports
 from rest_framework import serializers
 
@@ -5,13 +11,26 @@ from rest_framework import serializers
 from data import models
 
 
-class FixedClass(serializers.ModelSerializer):  # TODO remove Serialiser suffix
-    """Serialiser for the FixedClass model"""
+class FixedClass(serializers.ModelSerializer):
+    """Serialiser for the FixedClass model, implementing all model fields"""
     class Meta:
         model = models.FixedClass
         fields = ["school", "class_id", "subject_name", "teacher", "classroom", "pupils", "time_slots", "user_defined"]
 
 
-# TODO UnsolvedClassSerialiser
+class UnsolvedClass(serializers.ModelSerializer):
+    """
+    Serialiser for the UnsolvedClass model, implementing all model fields except 'school' which is not needed since
+    unsolved classes will only ever be transferred to the solver and not Posted by the solver.
+    """
+    class Meta:
+        model = models.UnsolvedClass
+        fields = ["class_id", "subject_name", "teacher", "pupils", "classroom", "total_slots", "min_distinct_slots"]
 
-# TODO TimetableSlotSerialiser
+
+class TimetableSlot(serializers.ModelSerializer):
+    """Serialiser for the TimetableSlot model, implementing all model fields except 'school' which is not needed since
+    unsolved classes will only ever be transferred to the solver and not Posted by the solver."""
+    class Meta:
+        model = models.TimetableSlot
+        fields = ["slot_id", "day_of_week", "period_starts_at", "period_duration"]
