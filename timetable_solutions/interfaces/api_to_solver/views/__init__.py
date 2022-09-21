1c692134c834d9e4189c8aea0a4933915ae2611f
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-21 06:58:48 +0100

    Defined ViewSets for the UnsolvedClass and TimetableSlot ViewSets

--- timetable_solutions/interfaces/api_to_solver/views/__init__.py
+++ timetable_solutions/interfaces/api_to_solver/views/__init__.py
@@ -1,2 +1,4 @@
 """Convenience imports of all model ViewSets"""
 from .fixed_class_view_set import FixedClass
+from .unsolved_class_view_set import UnsolvedClass
+from .timetable_slot_view_set import TimetableSlot
--- timetable_solutions/interfaces/api_to_solver/views/unsolved_class_view_set.py
+++ timetable_solutions/interfaces/api_to_solver/views/unsolved_class_view_set.py
@@ -1,1 +1,30 @@
-# TODO UnsolvedClassViewSet; http_method_names = ["get"]
+# Third party imports
+from rest_framework import viewsets
+
+# Local application imports
+from data import models
+from interfaces.api_to_solver import serialisers
+
+
+class UnsolvedClass(viewsets.ModelViewSet):
+    """
+    ViewSet for the UnsolvedClass - which the solver needs to be able to GET to understand what it is trying to
+    solve.
+    """
+    serializer_class = serialisers.UnsolvedClass
+    http_method_names = ["get"]
+
+    def get_queryset(self, *args, **kwargs):
+        """Customisation of the get_queryset method to allow access to a specific school's UnsolvedClass instances"""
+        school_access_key = self.request.query_params.get("school_access_key")
+        if school_access_key is not None:
+            try:
+                school_access_key = int(school_access_key)
+                unsolved_classes = models.UnsolvedClass.objects.get_all_school_unsolved_classes(
+                    school_id=school_access_key)
+                return unsolved_classes
+            except ValueError:
+                # TODO in this instance ensure response status code is 204
+                return None
+        else:
+            return None
