64a2c6e910c8463f1d969820fd0b364e76d41f07
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-22 08:36:18 +0100

    Updated url construction

--- timetable_solutions/domain/solver/constants/api_endpoints.py
+++ timetable_solutions/domain/solver/constants/api_endpoints.py
@@ -6,11 +6,19 @@ from dataclasses import dataclass
 
 @dataclass
 class DataLocation:
-    """Dataclass creating and storing absolute urls to where each piece of data relevant to the solver is located."""
+    """
+    Dataclass creating and storing absolute urls to where each piece of data relevant to the solver is located.
+    Domain name and protocol can be both set to '' for testing purposes.
+    """
     school_access_key: int
-    domain_name: str = "http://127.0.0.1:8000/"
+    domain_name: str = "127.0.0.1:8000"  # Locally hosted for now
+    protocol: str = "http"
 
     def __post_init__(self):
-        self.fixed_classes_url = f"{self.domain_name}/api/fixedclasses/?school_access_key={self.school_access_key}"
-        self.unsolved_classes_url = f"{self.domain_name}/api/unsolvedclasses?school_access_key={self.school_access_key}"
-        self.timetable_slots_url = f"{self.domain_name}/api/timetableslots?school_access_key={self.school_access_key}"
+        """Construct the full urls from the passed data"""
+        self.fixed_classes_url = f"{self.protocol}://{self.domain_name}/" \
+                                 f"api/fixedclasses/?school_access_key={self.school_access_key}"
+        self.unsolved_classes_url = f"{self.protocol}://{self.domain_name}" \
+                                    f"/api/unsolvedclasses?school_access_key={self.school_access_key}"
+        self.timetable_slots_url = f"{self.protocol}://{self.domain_name}" \
+                                   f"/api/timetableslots?school_access_key={self.school_access_key}"
