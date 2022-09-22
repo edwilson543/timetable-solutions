89b38eb3bd7b986160d183979f7830ed57378bbc
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-22 08:40:26 +0100

    Updated url construction and specification in solver

--- timetable_solutions/domain/solver/constants/api_endpoints.py
+++ timetable_solutions/domain/solver/constants/api_endpoints.py
@@ -8,17 +8,15 @@ from dataclasses import dataclass
 class DataLocation:
     """
     Dataclass creating and storing absolute urls to where each piece of data relevant to the solver is located.
-    Domain name and protocol can be both set to '' for testing purposes.
+    Protocol and domain name can be both set to '' for testing purposes.
     """
     school_access_key: int
-    domain_name: str = "127.0.0.1:8000"  # Locally hosted for now
-    protocol: str = "http"
+    protocol_domain: str = "http://127.0.0.1:8000"  # Locally hosted for now
 
     def __post_init__(self):
         """Construct the full urls from the passed data"""
-        self.fixed_classes_url = f"{self.protocol}://{self.domain_name}/" \
-                                 f"api/fixedclasses/?school_access_key={self.school_access_key}"
-        self.unsolved_classes_url = f"{self.protocol}://{self.domain_name}" \
-                                    f"/api/unsolvedclasses?school_access_key={self.school_access_key}"
-        self.timetable_slots_url = f"{self.protocol}://{self.domain_name}" \
-                                   f"/api/timetableslots?school_access_key={self.school_access_key}"
+        self.fixed_classes_url = f"{self.protocol_domain}/api/fixedclasses/?school_access_key={self.school_access_key}"
+        self.unsolved_classes_url = f"{self.protocol_domain}/api/" \
+                                    f"unsolvedclasses?school_access_key={self.school_access_key}"
+        self.timetable_slots_url = f"{self.protocol_domain}/api/" \
+                                   f"timetableslots?school_access_key={self.school_access_key}"
--- timetable_solutions/domain/solver/run_solver.py
+++ timetable_solutions/domain/solver/run_solver.py
@@ -10,18 +10,18 @@ from domain.solver.linear_programming.solver import TimetableSolver
 from domain.solver.linear_programming.solver_output_data import TimetableSolverOutcome
 
 
-def produce_timetable_solutions(school_access_key: int, domain_name: str | None = None) -> None | List[str]:
+def produce_timetable_solutions(school_access_key: int, protocol_domain: str | None = None) -> None | List[str]:
     """
     Function to be used by the web app to produce the timetable solutions.
     A button is clicked, which corresponds to a view, where that view calls this function.
     :param school_access_key - the unique integer used to access a given school's data via the API
-    :param domain_name - the domain name of the main web application using this solver
+    :param protocol_domain - the protocol and domain name of the main web application using this solver
     """
     # Locate the API endpoints for the necessary data
-    if domain_name is None:
+    if protocol_domain is None:
         data_location = DataLocation(school_access_key=school_access_key)
     else:
-        data_location = DataLocation(school_access_key=school_access_key, domain_name=domain_name)
+        data_location = DataLocation(school_access_key=school_access_key, protocol_domain=protocol_domain)
     input_data = TimetableSolverInputs(data_location=data_location)
 
     # Formulate the linear programming problem and try to solve
