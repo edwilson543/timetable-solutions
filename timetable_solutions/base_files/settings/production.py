7d67d699edcfd106b659365fee16f1a0f496b68c
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-26 19:44:57 +0100

    Added secret key to elasticbeanstalk and then extracted from os

--- timetable_solutions/base_files/settings/production.py
+++ timetable_solutions/base_files/settings/production.py
@@ -1,17 +1,18 @@
 """Settings specific to the aws production environment"""
 
-# Third party imports
-from decouple import config
+# Standard library imports
+import os
 
 # Local application imports
 from .base_settings import *
 
 # Per django security warning, the secret key is loaded in from an environment variable
-SECRET_KEY = config("SECRET_KEY")
+# The SECRET_KEY environment variable has been set on the elastic beanstalk environment
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Per Django security warning, debug is turned off for production!
 DEBUG = False
 
-ALLOWED_HOSTS = []  # TODO add domain name
+ALLOWED_HOSTS = ["timetable-solutions.eu-west-2.elasticbeanstalk.com"]  # TODO add domain name
 
 DATABASES = {}  # TODO
