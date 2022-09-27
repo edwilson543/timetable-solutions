02c10d4e83fe45ee7c45a7da0bf09a8f6ed94cd8
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-27 06:08:12 +0100

    Added a static  root

--- timetable_solutions/base_files/settings/base_settings.py
+++ timetable_solutions/base_files/settings/base_settings.py
@@ -102,9 +102,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
    BASE_DIR / 'base_files' / 'static',
-   ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
