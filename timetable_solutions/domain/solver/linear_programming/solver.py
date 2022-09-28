ff45a1845ff3202549dc9e5ec38e51831fe94181
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-28 08:38:43 +0100

    Added a bit of a sketch of what the solver and solver constraints class are going to look like

--- timetable_solutions/domain/solver/linear_programming/solver.py
+++ timetable_solutions/domain/solver/linear_programming/solver.py
@@ -1,12 +1,20 @@
+# Third party imports
+import pulp as lp
+
 # Local application imports
 from domain.solver.linear_programming.solver_input_data import TimetableSolverInputs
+from domain.solver.linear_programming.solver_variables import TimetableSolverVariables
 
 
-class TimetableSolver:
+class TimetableSolver(lp.LpProblem):
     """
     Class to formulate and solve the timetable scheduling problem as a linear programming problem
     Likely will end up wanting separate classes for different components of the problem formulation.
     """
-    def __init__(self, input_data: TimetableSolverInputs):
-        self.input_data = input_data
-        pass
+    def __init__(self,
+                 input_data: TimetableSolverInputs,
+                 variables: TimetableSolverVariables,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._input_data = input_data
+        self._variables = variables
