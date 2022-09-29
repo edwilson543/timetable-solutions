fdb235a30c0bdf8723e8b8281f99f865ddebe085
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-29 20:18:21 +0100

    Defined a namedtuple object for the variable dictionary's keys

--- timetable_solutions/domain/solver/linear_programming/__init__.py
+++ timetable_solutions/domain/solver/linear_programming/__init__.py
@@ -1,4 +1,4 @@
 from .solver_input_data import TimetableSolverInputs
-from .solver_variables import TimetableSolverVariables
+from .solver_variables import TimetableSolverVariables, variables_key
 from .solver import TimetableSolver
 from .solver_output_data import TimetableSolverOutcome
