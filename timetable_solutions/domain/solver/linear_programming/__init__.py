ff4e755c2b77f560f1a82f8b5f43e14db8985f6c
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-30 08:52:13 +0100

    Added and tested a method for instantiating the constraint that each pupil can only be in one place at a time

--- timetable_solutions/domain/solver/linear_programming/__init__.py
+++ timetable_solutions/domain/solver/linear_programming/__init__.py
@@ -1,4 +1,5 @@
 from .solver_input_data import TimetableSolverInputs
-from .solver_variables import TimetableSolverVariables, variables_key
+from .solver_variables import TimetableSolverVariables, var_key
+from .solver_constraints import TimetableSolverConstraints
 from .solver import TimetableSolver
 from .solver_output_data import TimetableSolverOutcome
