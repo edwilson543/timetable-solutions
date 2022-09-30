288e2505a66b21df41afd4935a4042c988c923d9
edwilson543 <edward.e.j.wilson@gmail.com>
2022-09-30 08:53:02 +0100

    Renamed variables_key -> var_key for brevity

--- timetable_solutions/domain/solver/linear_programming/solver_variables.py
+++ timetable_solutions/domain/solver/linear_programming/solver_variables.py
@@ -11,7 +11,7 @@ import pulp as lp
 from domain.solver.linear_programming.solver_input_data import TimetableSolverInputs
 
 
-variables_key = namedtuple("variables_key", "class_id slot_id")  # To be used as the keys of the dictionary
+var_key = namedtuple("var_key", "class_id slot_id")  # To be used as the keys of the dictionary
 
 
 class TimetableSolverVariables:
@@ -23,7 +23,7 @@ class TimetableSolverVariables:
     def __init__(self, inputs: TimetableSolverInputs):
         self._inputs = inputs
 
-    def get_variables(self, strip: bool = True) -> Dict[variables_key, lp.LpVariable]:
+    def get_variables(self, strip: bool = True) -> Dict[var_key, lp.LpVariable]:
         """
         Method to get the pulp variables relevant to a given solution.
         For each (unsolved class, timetable slot) pair, there is a binary variable indicating whether that class happens
@@ -31,7 +31,7 @@ class TimetableSolverVariables:
         :return - A dictionary of pulp variables, indexed by unique class / timetable slot tuples
         """
         variables = {
-            variables_key(class_id=unsolved_class.class_id, slot_id=timetable_slot.slot_id): lp.LpVariable(
+            var_key(class_id=unsolved_class.class_id, slot_id=timetable_slot.slot_id): lp.LpVariable(
                 f"{unsolved_class.class_id}_occurs_at_slot_{timetable_slot.slot_id}", cat="Binary") for
             unsolved_class in self._inputs.unsolved_classes for timetable_slot in self._inputs.timetable_slots
         }
@@ -39,7 +39,7 @@ class TimetableSolverVariables:
             self._strip_variables(variables=variables)
         return variables
 
-    def _strip_variables(self, variables: Dict[variables_key, lp.LpVariable]) -> None:
+    def _strip_variables(self, variables: Dict[var_key, lp.LpVariable]) -> None:
         """
         Method to remove variables corresponding to classes that are already known to occur at a certain time, since we
         know their value must be 1, so do not want to slow down the solver unnecessarily.
@@ -48,7 +48,7 @@ class TimetableSolverVariables:
         """
         for fixed_class in self._inputs.fixed_classes:
             for timetable_slot in fixed_class.time_slots.all():
-                variable_key = variables_key(class_id=fixed_class.class_id, slot_id=timetable_slot.slot_id)
+                variable_key = var_key(class_id=fixed_class.class_id, slot_id=timetable_slot.slot_id)
                 if variable_key in variables.keys():
                     # No need to access the timetable_slot's slot_id, since this is how it's stored on the FixedClass
                     variables.pop(variable_key)
