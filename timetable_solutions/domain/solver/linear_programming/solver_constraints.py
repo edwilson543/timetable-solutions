28b2938bde8eec9e5558fc9b0c14652fc2d7f5ed
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-03 07:10:50 +0100

    Added constraint that the teachers can only teach one class at a time

--- timetable_solutions/domain/solver/linear_programming/solver_constraints.py
+++ timetable_solutions/domain/solver/linear_programming/solver_constraints.py
@@ -38,18 +38,15 @@ class TimetableSolverConstraints:
         """
 
         def one_place_at_a_time_constraint(
-                pupil: models.Pupil, time_slot: models.TimetableSlot) -> Tuple[lp.LpConstraint, str] | None:
+                pupil: models.Pupil, time_slot: models.TimetableSlot) -> Tuple[lp.LpConstraint, str]:
             """
-            Defines the one-class-at-a-time constraint for and individual pupil, and individual timeslot.
-            :param pupil - represented by their id number, an integer
-            :param time_slot - represented by the slot_id number, an integer
-
-            We sum the variables at a fixed time slot over the unsolved classes, but then force this to 0 if the pupil
-            already has an existing commitment
+            Defines the one-class-at-a-time constraint for an individual pupil, and individual timeslot.
+            We sum the unsolved class variables relevant to the pupil, and force this to 0 if the pupil has an
+            existing commitment at the fixed time slot. Otherwise their sum must be max 1.
             """
             existing_commitment = pupil.check_if_busy_at_time_slot(slot=time_slot)
             possible_commitments = lp.lpSum(
-                [self._variables[key] for usc in self._inputs.unsolved_classes if (pupil in usc.pupils.all()) and
+                [self._variables.get(key) for usc in self._inputs.unsolved_classes if (pupil in usc.pupils.all()) and
                  (key := var_key(class_id=usc.class_id, slot_id=time_slot.slot_id)) in self._variables.keys()]
             )
             if existing_commitment:
@@ -64,8 +61,35 @@ class TimetableSolverConstraints:
                        pupil in self._inputs.pupils for time_slot in self._inputs.timetable_slots)
         return constraints
 
-    def _get_all_teacher_constraints(self) -> Generator[lp.LpConstraint, None, None]:
-        pass
+    def _get_all_teacher_constraints(self) -> Generator[Tuple[lp.LpConstraint, str], None, None]:
+        """
+        Method defining the constraints that each teacher can only teach one class at a time,and returning these as a
+        generator that is iterated through once to add each constraint to the LpProblem
+        """
+
+        def one_place_at_a_time_constraint(
+                teacher: models.Teacher, time_slot: models.TimetableSlot) -> Tuple[lp.LpConstraint, str]:
+            """
+            Defines the one-class-at-a-time constraint for an individual teacher, and individual timeslot.
+            We sum the unsolved class variables relevant to the teacher, and force this to 0 if the teacher has an
+            existing commitment at the fixed time slot. Otherwise their sum must be max 1.
+            """
+            existing_commitment = teacher.check_if_busy_at_time_slot(slot=time_slot)
+            possible_commitments = lp.lpSum(
+                [self._variables.get(key) for usc in self._inputs.unsolved_classes if (teacher == usc.teacher) and
+                 (key := var_key(class_id=usc.class_id, slot_id=time_slot.slot_id)) in self._variables.keys()]
+            )
+            if existing_commitment:
+                constraint = (possible_commitments == 0,
+                              f"teacher_{teacher.teacher_id}_unavailable_at_{time_slot.slot_id}")
+            else:
+                constraint = (possible_commitments <= 1,
+                              f"teacher_{teacher.teacher_id}_available_at_{time_slot.slot_id}")
+            return constraint
+
+        constraints = (one_place_at_a_time_constraint(teacher=teacher, time_slot=time_slot) for
+                       teacher in self._inputs.teachers for time_slot in self._inputs.timetable_slots)
+        return constraints
 
     def _get_all_fulfillment_constraints(self) -> Generator[lp.LpConstraint, None, None]:
         pass
