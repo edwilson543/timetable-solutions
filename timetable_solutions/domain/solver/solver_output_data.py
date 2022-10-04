d0eadebc737d1743d5ed306e58d0e23c6f460125
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-04 09:03:04 +0100

    Defined prototypes for the methods by which the solver outputs leave memory and impact the database

--- timetable_solutions/domain/solver/solver_output_data.py
+++ timetable_solutions/domain/solver/solver_output_data.py
@@ -1,28 +1,56 @@
-# Standard library imports
-
 # Local application imports
+from data import models
 from domain.solver.linear_programming.solver import TimetableSolver
 
 
 class TimetableSolverOutcome:
     """
-    Class responsible for extracting results from a solved TimetableSolver, and submitting a POST request of these
-    back to the main web app
+    Class responsible for extracting results from a solved TimetableSolver, and inserting the outcome into the database.
     """
 
-    def __init__(self, timetable_solver: TimetableSolver):
+    def __init__(self,
+                 timetable_solver: TimetableSolver):
         self._timetable_solver = timetable_solver
         self._input_data = timetable_solver.input_data
         self._variables = timetable_solver.variables
-        self.error_messages = None
+        self.error_messages = timetable_solver.error_messages
 
-    def extract_and_post_results(self) -> None:
-        fixed_classes = self._extract_results()
-        self._save_results_to_db(fixed_classes=fixed_classes)
-        # TODO - above just sketch of what should happen
+    def _extract_results(self) -> None:
+        """
+        Method to recover the variable values (1s / 0s) and use these to create FixedClass instances, and or update
+        existing FixedClass instances already with some known time slots
+        """
+        for unsolved_class in self._input_data.unsolved_classes:
+            solved_timeslot_ids = [var_key.slot_id for var_key, var in self._variables.items() if var.varValue == 1.0]
+            solved_timeslots = self._input_data.timetable_slots.filter(slot_id__in=solved_timeslot_ids)
+            if solved_timeslots.count() == 0:
+                continue
 
-    def _extract_results(self):
-        pass
+            corresponding_fixed_class = self._input_data.fixed_classes.filter(class_id=unsolved_class.class_id)
+            if corresponding_fixed_class.count() == 1:
+                fixed_class = corresponding_fixed_class.first()
+                self._add_slots_to_existing_fixed_class(fixed_class=fixed_class, timeslots=solved_timeslots)
+            else:
+                self._create_new_fixed_class_from_time_slots(unsolved_class=unsolved_class, timeslots=solved_timeslots)
 
-    def _save_results_to_db(self, fixed_classes) -> None:
-        pass
+    @staticmethod
+    def _add_slots_to_existing_fixed_class(
+            fixed_class: models.FixedClass, timeslots: models.TimetableSlotQuerySet) -> None:
+        """
+        Method to update an existing fixed class' time_slots field, with the additional time slots that the solver
+        has produced. In this case, the user had defined a FixedClass with some required time slots.
+        """
+        fixed_class.add_time_slots(time_slots=timeslots)
+
+    @staticmethod
+    def _create_new_fixed_class_from_time_slots(
+            unsolved_class: models.UnsolvedClass, timeslots: models.TimetableSlotQuerySet) -> None:
+        """
+        Method to create an entirely new FixedClass instance, using an UnsolvedClass instance and the time slots
+        produced by the solver. In this case, the user had not defined any required slots via a FixedClass.
+        """
+        models.FixedClass.create_new(
+            school_id=unsolved_class.school.school_access_key, user_defined=False, class_id=unsolved_class.class_id,
+            pupils=unsolved_class.pupils, time_slots=timeslots, classroom_id=unsolved_class.classroom.classroom_id,
+            subject_name=unsolved_class.subject_name, teacher_id=unsolved_class.teacher.teacher_id
+        )
