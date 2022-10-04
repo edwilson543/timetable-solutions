8cf7b8e6625bd4fa678b032b4122054691b0a638
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-04 07:17:15 +0100

    Added a "test scenario 1" which checks the solver is finding the correct solution in a manufactured scenario

--- timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
+++ timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
@@ -7,7 +7,7 @@ from django import test
 import pulp as lp
 
 # Local application imports
-from domain.solver import linear_programming as l_p
+from domain import solver as slvr
 
 
 class TestSolver(test.TestCase):
@@ -15,23 +15,23 @@ class TestSolver(test.TestCase):
     Test that the solver can produce a solution for the default fixtures.
     These fixtures are not contrived to produce a specific solution, so we do not test what the solution actually is.
     """
+    fixtures = ["user_school_profile.json", "classrooms.json", "pupils.json", "teachers.json", "timetable.json",
+                "fixed_classes_lunch.json", "unsolved_classes.json"]
 
-    fixtures = ["user_school_profile.json", "solver_pupils.json"]
-
-    def test_add_all_constraints_to_problem(self):
+    def test_solver_finds_a_solution_for_default_fixtures(self):
         """
         Test that the full set of constraints necessary to specify the timetabling problem is added to the problem
         """
         # Set test parameters
         school_access_key = 123456
-        data = l_p.TimetableSolverInputs(school_id=school_access_key)
-        solver = l_p.TimetableSolver(input_data=data)
+        data = slvr.TimetableSolverInputs(school_id=school_access_key)
+        solver = slvr.TimetableSolver(input_data=data)
 
         # Execute test unit
         solver.solve()
 
         # Check outcome - i.e. that a solution has been found
-        self.assertTrue(lp.LpStatus[solver.problem.status], "Optimal")
+        assert lp.LpStatus[solver.problem.status] == "Optimal"
 
 
 class TestSolverScenarioSolutions(test.TestCase):
@@ -39,16 +39,24 @@ class TestSolverScenarioSolutions(test.TestCase):
     Tests where we are after a specific solution
     """
 
-    fixtures = ["user_school_profile.json", "solver_pupils.json"]
+    fixtures = ["user_school_profile.json", "test_scenario_1.json"]
 
-    def test_add_all_constraints_to_problem(self):
+    def test_solver_solution_test_scenario_1(self):
         """
-        Test that the full set of constraints necessary to specify the timetabling problem is added to the problem
+        Test scenario 1 essentially represents a test of the fulfillment constraint. There are 2 pupils / teachers /
+        timeslots / fixed classes / unsolved classes. Each fixed class occupies one of the slots, and the unsolved class
+        states 2 slots must be used, so the solution is just to occupy the remaining slot, for each class.
         """
         # Set test parameters
         school_access_key = 123456
-        # data = l_p.TimetableSolverInputs(school_id=school_access_key)
-        # solver = l_p.TimetableSolver(input_data=data)
-        #
-        # # Execute test unit
-        # solver.solve()
+        data = slvr.TimetableSolverInputs(school_id=school_access_key)
+        solver = slvr.TimetableSolver(input_data=data)
+
+        # Execute test unit
+        solver.solve()
+
+        # Check outcome
+        assert lp.LpStatus[solver.problem.status] == "Optimal"
+        assert len(solver.variables) == 2  # Only one option for where each unsolved class' remaining slots could go
+        assert solver.variables[slvr.var_key(class_id="YEAR_ONE_MATHS_A", slot_id=2)].varValue == 1
+        assert solver.variables[slvr.var_key(class_id="YEAR_ONE_MATHS_B", slot_id=1)].varValue == 1
