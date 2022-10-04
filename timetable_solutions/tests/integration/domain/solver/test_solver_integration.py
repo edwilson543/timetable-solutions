477071ebdd211397500b4afa7f8cbd7a97ab7eea
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-04 07:44:44 +0100

    Added test scenario 2, working as expected. Had to update test scenario 1 to  not shared any primary keys

--- timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
+++ timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
@@ -39,7 +39,7 @@ class TestSolverScenarioSolutions(test.TestCase):
     Tests where we are after a specific solution
     """
 
-    fixtures = ["user_school_profile.json", "test_scenario_1.json"]
+    fixtures = ["test_scenario_1.json", "test_scenario_2.json"]
 
     def test_solver_solution_test_scenario_1(self):
         """
@@ -48,7 +48,7 @@ class TestSolverScenarioSolutions(test.TestCase):
         states 2 slots must be used, so the solution is just to occupy the remaining slot, for each class.
         """
         # Set test parameters
-        school_access_key = 123456
+        school_access_key = 111111
         data = slvr.TimetableSolverInputs(school_id=school_access_key)
         solver = slvr.TimetableSolver(input_data=data)
 
@@ -60,3 +60,27 @@ class TestSolverScenarioSolutions(test.TestCase):
         assert len(solver.variables) == 2  # Only one option for where each unsolved class' remaining slots could go
         assert solver.variables[slvr.var_key(class_id="YEAR_ONE_MATHS_A", slot_id=2)].varValue == 1
         assert solver.variables[slvr.var_key(class_id="YEAR_ONE_MATHS_B", slot_id=1)].varValue == 1
+
+    def test_solver_solution_test_scenario_2(self):
+        """
+        Test scenario 2 represents a test of the pupil one-place-at-a-time constraint. There is one pupil, who must
+        go to 2 classes. There are 2 time slots. There are no fixed classes, or other constraints.
+        """
+        # Set test parameters
+        school_access_key = 222222
+        data = slvr.TimetableSolverInputs(school_id=school_access_key)
+        solver = slvr.TimetableSolver(input_data=data)
+
+        # Execute test unit
+        solver.solve()
+
+        # Check outcome
+        assert lp.LpStatus[solver.problem.status] == "Optimal"
+        assert len(solver.variables) == 4  # Either class could be taught in either slot
+        # We test now that the sufficient classes take place, and not simultaneously
+        c1_t1 = solver.variables[slvr.var_key(class_id="MATHS", slot_id=1)].varValue
+        c1_t2 = solver.variables[slvr.var_key(class_id="MATHS", slot_id=2)].varValue
+        c2_t1 = solver.variables[slvr.var_key(class_id="ENGLISH", slot_id=1)].varValue
+        c2_t2 = solver.variables[slvr.var_key(class_id="ENGLISH", slot_id=2)].varValue
+        assert c1_t1 + c1_t2 == 1
+        assert c2_t1 + c2_t2 == 1
