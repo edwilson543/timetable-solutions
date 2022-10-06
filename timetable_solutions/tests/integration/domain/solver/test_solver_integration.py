fc90f4b0ee9716871886f0e38471facf5f3ebaf6
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-06 08:33:45 +0100

    Added a sixth test scenario, used to test how a double period constraint pairs with having a fixed class. Which is working as expected

--- timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
+++ timetable_solutions/tests/integration/domain/solver/test_solver_integration.py
@@ -40,7 +40,7 @@ class TestSolverScenarioSolutions(test.TestCase):
     """
 
     fixtures = ["test_scenario_1.json", "test_scenario_2.json", "test_scenario_3.json", "test_scenario_4.json",
-                "test_scenario_5.json"]
+                "test_scenario_5.json", "test_scenario_6.json"]
 
     def test_solver_solution_test_scenario_1(self):
         """
@@ -146,6 +146,33 @@ class TestSolverScenarioSolutions(test.TestCase):
         assert lp.LpStatus[solver.problem.status] == "Optimal"
         assert len(solver.variables.decision_variables) == 3  # 1 unsolved class must be taught in 2 / 3 time slots
 
+        # The double period is slots 2 & 3
         assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=1)].varValue == 0
         assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=2)].varValue == 1
+        assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=3)].varValue == 1
+
+    def test_solver_solution_test_scenario_6(self):
+        """
+        Test scenario targeted at the double period fulfillment and dependency constraints, in the particular instance
+        where a FixedClass comes into play.
+        There are 4 timeslots, as 2 pairs of consecutive slots.
+        There is one UnsolvedClass that must have a total of 2 slots, which must be a double period, and
+        a corresponding FixedClass with 1 defined slot. Therefore the solution is to attach a second slot to the
+        already defined slot, making the double.
+        """
+        # Set test parameters
+        school_access_key = 666666
+        data = slvr.TimetableSolverInputs(school_id=school_access_key)
+        solver = slvr.TimetableSolver(input_data=data)
+
+        # Execute test unit
+        solver.solve()
+
+        # Check outcome
+        assert lp.LpStatus[solver.problem.status] == "Optimal"
+        assert len(solver.variables.decision_variables) == 3  # 4 slots, 1 class, but one variable gets stripped
+
+        # The fixed class is at slot 4, slots (1 & 2) and (3 & 4) are the consecutive pairs
+        assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=1)].varValue == 0
+        assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=2)].varValue == 0
         assert solver.variables.decision_variables[slvr.var_key(class_id="ENGLISH", slot_id=3)].varValue == 1
