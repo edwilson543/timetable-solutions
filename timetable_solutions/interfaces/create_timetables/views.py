16b02afef2359490349b29a7e056f5ad5300552e
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-12 06:43:51 +0100

    View for accessing the dashboard allowing users to create their own timetable

--- timetable_solutions/interfaces/create_timetables/views.py
+++ timetable_solutions/interfaces/create_timetables/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
 
-# Create your views here.
+# Django imports
+from django.http import HttpResponse
+from django import views
+from django.template import loader
+
+# Local application imports
+from interfaces.create_timetables import forms
+
+
+class CreateTimetable(views.View):
+    """
+    View relating to the 'dashboard' / homepage of the 'create' component on the wider application, which allows
+    users to initiate the creation of timetable solutions.
+    """
+    @staticmethod
+    def get(request, *args, **kwargs):
+        template = loader.get_template("create")
+        context = {
+            "form": forms.SolutionSpecification()
+        }
+        return HttpResponse(template.render(context, request))
+
+    def post(self, request, *args, **kwargs):
+        pass
