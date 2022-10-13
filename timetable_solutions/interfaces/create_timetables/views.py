d25ec9db264f17dad0c2447736a9b347b33cdda5
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-13 07:47:05 +0100

    Linked the running of the solver into the post request on the create timetable view

--- timetable_solutions/interfaces/create_timetables/views.py
+++ timetable_solutions/interfaces/create_timetables/views.py
@@ -1,10 +1,12 @@
 
 # Django imports
-from django.http import HttpResponse
 from django import views
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django import urls
 
 # Local application imports
+from domain.solver.run_solver import produce_timetable_solutions
 from interfaces.create_timetables import forms
 
 
@@ -22,4 +24,11 @@ class CreateTimetable(views.View):
         return HttpResponse(template.render(context, request))
 
     def post(self, request, *args, **kwargs):
-        pass
+        """This post method is used to initiate the running of the solver - the core service of the app."""
+        form = forms.SolutionSpecification(request.POST)
+        if form.is_valid():
+            school_access_key = request.user.profile.school.school_access_key
+            produce_timetable_solutions(school_access_key=school_access_key)
+            return HttpResponseRedirect(urls.reverse("selection_dashboard"))
+        else:
+            return self.get(request, *args, **kwargs)
