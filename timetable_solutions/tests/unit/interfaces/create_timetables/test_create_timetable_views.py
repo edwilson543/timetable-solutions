c4f86d9a757535602c876dd39c965b7d7cb770ec
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-16 07:53:38 +0100

    Added a test for a POST request to the create timetable view, with a more complex run scenario

--- timetable_solutions/tests/unit/interfaces/create_timetables/test_create_timetable_views.py
+++ timetable_solutions/tests/unit/interfaces/create_timetables/test_create_timetable_views.py
@@ -30,10 +30,10 @@ class TestCreateTimetableFormView(test.TestCase):
         assert response.status_code == 200
         assert isinstance(response.context["form"], forms.SolutionSpecification)
 
-    def test_post_method_runs_solver_with_solution_spec_form_using_default_options(self):
+    def test_post_method_runs_solver_with_solution_spec_form_using_simplest_run_options(self):
         """
         Test that by submitting a POST request to the create timetable url, with a SolutionSpecification form that uses
-        the defaults (see below), the solver is run.
+        simple run requirements (i.e. allows all simplifications), the solver is run successfully.
         """
         # Set test parameters
         self.client.login(username="dummy_teacher", password="dt123dt123")
@@ -58,5 +58,32 @@ class TestCreateTimetableFormView(test.TestCase):
             if not fc.user_defined:  # i.e. this fc was produced by the solver
                 assert fc.time_slots.count() == 8  # per UnsolvedClass requirements
 
+    def test_post_method_runs_solver_with_solution_spec_using_default_options(self):
+        """
+        Test that by submitting a POST request to the create timetable url, with a SolutionSpecification form that
+        forces all components (i.e. constraints / objective) of the solver to be uses, the solver is run successfully.
+        """
+        # Set test parameters
+        self.client.login(username="dummy_teacher", password="dt123dt123")
+        url = urls.reverse("create_timetables")
+        form_data = {
+            "allow_split_classes_within_each_day": False,
+            "allow_triple_periods_and_above": False,
+        }
+        expected_url_redirect = urls.reverse("selection_dashboard")
+
+        # Execute test unit
+        response = self.client.post(url, data=form_data)
+
+        # Check outcome
+        self.assertRedirects(response=response, expected_url=expected_url_redirect)
+        assert response.status_code == 302  # Redirection
+
+        # Check that a solution has been produced
+        fixed_classes = models.FixedClass.objects.get_all_instances_for_school(school_id=123456)
+        assert fixed_classes.count() == 24  # 12 user defined, 12 from solver
+        for fc in fixed_classes:
+            if not fc.user_defined:  # i.e. this fc was produced by the solver
+                assert fc.time_slots.count() == 8  # per UnsolvedClass requirements
 
     # TODO - combine the class with django.contrib.auth.mixins LoginRequiredMixin and test
