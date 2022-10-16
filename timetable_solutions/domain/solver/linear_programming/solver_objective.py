b3724852d3a390572bd330b31031f02fe5bbc499
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-16 13:10:19 +0100

    Added an initial method for finding the objective contribution from optimising free period timing - as of yet not tested

--- timetable_solutions/domain/solver/linear_programming/solver_objective.py
+++ timetable_solutions/domain/solver/linear_programming/solver_objective.py
@@ -2,6 +2,9 @@
 Module defining the objective function of the timetable solving problem
 """
 
+# Standard library imports
+import datetime as dt
+
 # Third party imports
 import pulp as lp
 
@@ -33,13 +36,41 @@ class TimetableSolverObjective:
         """
         pass
 
-    def _sum_all_objective_components(self) -> lp.LpAffineExpression:
+    def _find_and_sum_all_objective_components(self) -> lp.LpAffineExpression:
         """
         Method to gather each component of the objective function, and sum the together.
         :return - total_objective - the objective function of the given school timetabling problem.
         """
-        pass
+        objective = lp.LpAffineExpression()
+        if self._inputs.solution_specification.optimal_free_period_time_of_day is not None:
+            objective += self._get_free_period_timing_objective()
+
+        return objective
 
     # OBJECTIVE COMPONENTS
-    def _free_period_timing_objective(self) -> lp.LpAffineExpression:
-        pass
+    def _get_free_period_timing_objective(self) -> lp.LpAffineExpression:
+        """
+        Objective component relating to the total distance in time between each class that takes place and the time the
+        user has specified as optimal for free periods to happen.
+
+        The effect is to, within the constraints, push the classes away from the slot corresponding to the optimal free
+        period slot - i.e. the optimal free period time acts like an opposing magnet to all the decision variables.
+
+        :return - objective_component - the total duration of time between the optimal free time slot and each
+        decision variable.
+        """
+        objective_component = lp.LpAffineExpression()
+
+        repulsive_time = self._inputs.solution_specification.optimal_free_period_time_of_day
+        repulsive_time_as_delta = dt.timedelta(hours=repulsive_time.hour)
+
+        for key, var in self._decision_variables.items():
+            slot_time = self._inputs.get_time_period_starts_at_from_slot_id(slot_id=key.slot_id)
+            slot_time_as_delta = dt.timedelta(hours=slot_time.hour)
+            difference_hours = abs((slot_time_as_delta - repulsive_time_as_delta).seconds / 3600)
+
+            # If the associated class takes place at this time (i.e. var = 1), we will get a non-zero contribution below
+            contribution = difference_hours * var
+            objective_component += contribution
+
+        return objective_component
