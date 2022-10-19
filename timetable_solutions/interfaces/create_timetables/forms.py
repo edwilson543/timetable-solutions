592b24de137bf528d8ffd7cec29ae0c3cd8c90a8
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-19 19:57:27 +0100

    Added some more styling to the create timetable page

--- timetable_solutions/interfaces/create_timetables/forms.py
+++ timetable_solutions/interfaces/create_timetables/forms.py
@@ -24,7 +24,7 @@ class SolutionSpecification(forms.Form):
 
     # Form fields
     allow_split_classes_within_each_day = forms.BooleanField(
-        label="Allow each class to be taught more than once in a day", label_suffix="", widget=forms.CheckboxInput,
+        label="Classes can be taught at separate times in a day", label_suffix="", widget=forms.CheckboxInput,
         required=False)
     allow_triple_periods_and_above = forms.BooleanField(
         label="Allow triple periods or longer", label_suffix="", widget=forms.CheckboxInput, required=False)
