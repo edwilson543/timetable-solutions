f015a74ef59818fac49ca8ee2d542093b3aee9a1
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-24 08:48:18 +0100

    Deleted duplicate test (left over from splitting out some of the core lofic)

--- timetable_solutions/tests/unit/domain/view_timetables/test_data_pre_processing.py
+++ timetable_solutions/tests/unit/domain/view_timetables/test_data_pre_processing.py
@@ -10,7 +10,6 @@ from django.db.models import QuerySet
 from django.test import TestCase
 
 # Local application imports
-import domain.view_timetables.timetable_summary_stats
 from data import models
 from domain import view_timetables
 
@@ -19,16 +18,6 @@ class TestDataPreProcessing(TestCase):
     """Test class for the all functions in the data_pre_processing module of the view_timetables in the domain."""
     fixtures = ["user_school_profile.json", "classrooms.json", "pupils.json", "teachers.json", "timetable.json",
                 "fixed_classes.json"]
-
-    def test_get_summary_stats_for_dashboard_correct(self):
-        """Test that the correct summary stats are produced for the test fixtures."""
-        stats = domain.view_timetables.timetable_summary_stats.get_summary_stats_for_dashboard(school_access_key=123456)
-        # All assertion values are evident from the tests fixtures
-        self.assertEqual(stats["total_classes"], 12)
-        self.assertEqual(stats["total_lessons"], 100)
-        self.assertEqual(stats["total_pupils"], 6)
-        self.assertEqual(stats["total_teachers"], 11)
-        self.assertIsInstance(stats["busiest_slot"], models.TimetableSlot)
 
     def test_get_year_indexed_pupils(self):
         """Test that the correct full list of pupils indexed by year group is returned"""
