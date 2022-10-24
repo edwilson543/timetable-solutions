d0e83ffee55a32be0b6f76a6f56f854f5f95c5c7
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-24 18:43:15 +0100

    Added functionality to not display the create timetable form if the user has uploaded insufficient data, and unit tested this.

--- timetable_solutions/tests/unit/interfaces/create_timetables/test_create_timetable_views.py
+++ timetable_solutions/tests/unit/interfaces/create_timetables/test_create_timetable_views.py
@@ -22,11 +22,13 @@ class TestCreateTimetableFormView(test.TestCase):
     fixtures = ["user_school_profile.json", "timetable.json"]
 
     def test_get_form_kwargs_contains_correct_available_time_slots(self):
-        """Unit test for the method over-riding FormView's get_form_kwargs method"""
+        """
+        Unit test for the method over-riding FormView's get_form_kwargs method
+        """
         # Set test parameters
         factory = test.RequestFactory()
         request = factory.get(urls.reverse(UrlName.CREATE_TIMETABLES.value))
-        request.user = User.objects.get(pk=1)
+        request.user = User.objects.get(pk=1)  # Dummy teacher has pk=1
         view = views.CreateTimetable(request=request)
 
         # Execute test unit
@@ -35,3 +37,22 @@ class TestCreateTimetableFormView(test.TestCase):
         # Check outcome
         available_slots = kwargs["available_time_slots"]
         assert available_slots == [dt.time(hour=hour) for hour in range(9, 16)]
+
+    def test_get_context_dictionary_ready_to_create_false_as_fixtures_are_sparse(self):
+        """
+        Unit test for the method over-riding FormMixin's get_context_data method.
+        Note that this TestCase uses insufficient fixtures to fully define the timetabling problem (analogous to the
+        user having uploaded insufficient data), and therefore we expect the ready_to_create variable to be False.
+        """
+        # Set test parameters
+        factory = test.RequestFactory()
+        request = factory.get(urls.reverse(UrlName.CREATE_TIMETABLES.value))
+        request.user = User.objects.get(pk=1)  # Dummy teacher has pk=1
+        view = views.CreateTimetable(request=request)
+
+        # Execute test unit
+        context_dict = view.get_context_data()
+
+        # Check outcome
+        ready_to_create = context_dict["ready_to_create"]
+        assert not ready_to_create
