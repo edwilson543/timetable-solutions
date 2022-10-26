7ba76a901a067c8cb4815e2d104fe466bc8793ca
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-26 08:00:00 +0100

    Tidied docstring and added TimetableColour to conveience imports

--- timetable_solutions/domain/view_timetables/__init__.py
+++ timetable_solutions/domain/view_timetables/__init__.py
@@ -1,5 +1,8 @@
-# Convenience imports
+"""
+Convenience imports for objects form view_timetables domain component
+"""
 from .timetable_constructor import (
     get_year_indexed_pupils, get_letter_indexed_teachers,
     get_pupil_timetable_context, get_teacher_timetable_context)
 from .timetable_summary_stats import get_summary_stats_for_dashboard
+from .timetable_colours import TimetableColour
