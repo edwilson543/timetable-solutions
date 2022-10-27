927de2d31d1a1ad43890bb9882047d154b097c10
edwilson543 <edward.e.j.wilson@gmail.com>
2022-10-27 19:02:43 +0100

    Updated the TimetableColour class to be a little bit smarter in that it matches some strings with regexes, rather than relying on exact string matches

--- timetable_solutions/domain/view_timetables/timetable_colours.py
+++ timetable_solutions/domain/view_timetables/timetable_colours.py
@@ -4,7 +4,8 @@ Module defining the constant values (hexadecimal colour codes) assigned to subje
 
 # Standard library imports
 from enum import Enum
-from typing import Dict, Union
+import re
+from typing import Dict, Union, Pattern
 
 # Third party imports
 import pandas as pd
@@ -15,33 +16,56 @@ from data import models
 
 class TimetableColour(Enum):
     """
-    Enumeration of the hexadecimal colours to use for the timetables in the UI. For certain subject types
-    (lunch / free), a specific colour is given, otherwise, we assign colours based on the most periods. The Enum also
-    therefore specifies all subjects that we may want to reference in the code (e.g. lunch / break etc.)
+    Enumeration of the hexadecimal colours to use for colour coding the timetables in the UI. For certain period types
+    (meals / free), a specific colour is given, otherwise, we assign colours based on the most periods / week.
 
-    This enum also has methods defining the logic to assign the colours to use when rendering a pupil / teacher's
-    timetable.
+    The class also has methods defining the logic to assign the colour codes to use when rendering a pupil / teacher's
+    timetable, to school class name strings
     """
-    # Unranked subject colours
-    LUNCH = "#b3b3b3"  # Light grey
+    # Generic period type colours - assigned using the regex dict defined below
+    MEAL = "#b3b3b3"  # Light grey
     BREAK = "#bfbfbf"  # Slightly darker grey
     FREE = "#feffba"  # Yellow
 
-    # Ranked subject colours - to be assigned to subjects, based on weekly frequency
-    # Or in the case of teacher timetables, the numbers represent year groups
-    COLOUR_RANKING = {
-        0: "#ffbfd6",  # Pale red
-        1: "#c8d4e3",  # Pale blue
-        2: "#b3f2b3",  # Pale green
-        3: "#d0bad6",  # Lilac
-        4: "#e3ad62",  # Pale orange
-        5: "#675fb0",  # Dark blue
-        6: "#aabf5e",  # Muddy green
-        7: "#8f7e78",  # Pink-grey
-        8: "#d18771",  # Dark red
-        9: "#169c6f",  # Turquoise
-        10: "#c9ae61",  # Light brown
-    }
+    @staticmethod
+    def _get_colour_ranking() -> Dict[int, str]:
+        """
+        Method returning a constant dictionary (equivalently an enum.property in python 3.11, where as this project uses
+        python 3.8)
+        :return: Ranked subject colours - to be assigned to subjects, based on weekly frequency in pupil timetables,
+        or in the case of teacher timetables, the numbers represent year groups.
+        """
+        colour_ranking = {
+            0: "#ffbfd6",  # Pale red
+            1: "#c8d4e3",  # Pale blue
+            2: "#b3f2b3",  # Pale green
+            3: "#d0bad6",  # Lilac
+            4: "#e3ad62",  # Pale orange
+            5: "#675fb0",  # Dark blue
+            6: "#aabf5e",  # Muddy green
+            7: "#8f7e78",  # Pink-grey
+            8: "#d18771",  # Dark red
+            9: "#169c6f",  # Turquoise
+            10: "#c9ae61",  # Light brown
+        }
+        return colour_ranking
+
+    @classmethod
+    def _get_regex_dict(cls) -> Dict[Pattern[str], str]:
+        """
+        Method returning a constant dictionary (equivalently an enum.property in python 3.11, where as this project uses
+        python 3.8)
+        :return dictionary whose keys are compiled regex patterns, and the values are colour codes
+        (accessed via the Enum member values). This dict is used to match against user input strings, to see if any of
+        their input classes should be assigned a default colour
+        """
+        regex_dict = {
+            re.compile("(.*breakfast.*)|(.*lunch.*)|(.*tea.*)|(.*dinner.*)|(.*supper.*)",
+                       flags=re.IGNORECASE): cls.MEAL.value,
+            re.compile("(.*break.*)|(.*recreation.*)|(.*play.*time.*)", flags=re.IGNORECASE): cls.BREAK.value,
+            re.compile("(.*free.*)|(.*empty.*)|(.*spare.*)", flags=re.IGNORECASE): cls.FREE.value,
+        }
+        return regex_dict
 
     @classmethod
     def get_colours_for_pupil_timetable(cls, classes: models.FixedClassQuerySet) -> Dict[str, str]:
@@ -51,24 +75,25 @@ class TimetableColour(Enum):
         :return A dictionary whose keys are subject names, and values are corresponding hexadecimal colour codes
         """
         # Create dictionary of fixed period name -> hexadecimal colour mappings
-        unranked_colours = cls._get_unranked_colours()
+        generic_period_colours = cls._get_generic_period_colours(classes=classes)
+
+        colour_ranking = cls._get_colour_ranking()  # To be used throughout remainder of method
 
         # Count the instances of each subject per week, removing any that are already in class_colours
         uncleaned_class_counts = {klass.subject_name: klass.time_slots.all().count() for klass in classes}
         class_counts = {subject_name: count for subject_name, count in uncleaned_class_counts.items() if
-                        subject_name not in unranked_colours.keys()}
+                        subject_name not in generic_period_colours.keys()}
 
         # Rank the classes ensuring all final ranks are in the specified colour ranking
-        counts_ser = pd.Series(class_counts)  # Create a series to use the rank functionality
+        counts_ser = pd.Series(class_counts)  # Series used to access the .rank() method
         rank_ser = counts_ser.rank(method="first", ascending=False) - 1  # -1 so that ranks start at 0
-        max_defined_rank = max(cls.COLOUR_RANKING.value.keys())
+        max_defined_rank = max(colour_ranking.keys())
         rank_ser = rank_ser.mod(max_defined_rank)
         class_rank_dict = rank_ser.to_dict()
 
         # Assign each subject a colour based on rank
-        ranked_colours = {subject_name: cls.COLOUR_RANKING.value[rank] for
-                          subject_name, rank in class_rank_dict.items()}
-        class_colours = unranked_colours | ranked_colours
+        ranked_colours = {subject_name: colour_ranking[rank] for subject_name, rank in class_rank_dict.items()}
+        class_colours = {**generic_period_colours, **ranked_colours}
         return class_colours
 
     @classmethod
@@ -80,27 +105,46 @@ class TimetableColour(Enum):
         :return A dictionary whose keys are subject names, and values are corresponding hexadecimal colour codes
         """
         # Create dictionary of fixed period name -> hexadecimal colour mappings
-        unranked_colours = cls._get_unranked_colours()
+        generic_period_colours = cls._get_generic_period_colours(classes=classes)
 
+        colour_ranking = cls._get_colour_ranking()
         year_group_colours = {}  # Dictionary that will map year group colours -> ints
         for klass in classes:
             all_pupils = klass.pupils.all()
             if all_pupils.exists():  # Take first pupil from queryset since all have same year group
                 first_pupil = klass.pupils.all().first()
                 year_group: int = first_pupil.year_group
-                year_group_colours[year_group] = cls.COLOUR_RANKING.value[year_group]
+                year_group_colours[year_group] = colour_ranking[year_group]
 
-        all_colours = unranked_colours | year_group_colours
+        all_colours = {**generic_period_colours, **year_group_colours}
         return all_colours
 
     # HELPER METHODS
     @classmethod
-    def _get_unranked_colours(cls) -> Dict[str, str]:
+    def _get_generic_period_colours(cls, classes: models.FixedClassQuerySet) -> Dict[str, str]:
         """
-        Method to get the dictionary of colours as subject_name: hexadecimal_colour_code, which are 'unranked', i.e.
-        all colours that are not in the COLOUR_RANKING dictionary
+        Method getting the generic period colours relevant to an individual timetable.
+        :param classes - the queryset of classes specific to an individual pupil / teacher
+        :return generic_period_colours -  dictionary of subject_name: hexadecimal_colour_codes, where the subject names
+        correspond to the generic period types identified in the classes parameter
         """
-        # TODO - if this method received classes as an argument, it could then uses regexes to match the subject names
-        #  to the enum names, and thereby the colours
-        unranked_colours = {colour.name: colour.value for colour in cls if colour is not cls.COLOUR_RANKING}
-        return unranked_colours
+        generic_period_colours = {
+            klass.subject_name: matched_colour_code for klass in classes if
+            (matched_colour_code := cls._check_class_for_colour_in_regex(class_name=klass.subject_name)) is not None
+        }
+        # Free periods are normally undefined (i.e. will NOT be in the classes arg), and are used to fill missing slots
+        # So this must be added manually
+        generic_period_colours[cls.FREE.name] = cls.FREE.value
+        return generic_period_colours
+
+    @classmethod
+    def _check_class_for_colour_in_regex(cls, class_name: str) -> Union[str, None]:
+        """
+        Method to check the class_name parameter to see if it matches any of the regexes in regex dict.
+        :return the colour code from the matched regex, if there is one, otherwise None.
+        """
+        regex_dict = cls._get_regex_dict()
+        for regex, colour_code in regex_dict.items():
+            is_match = bool(re.search(regex, string=class_name))
+            if is_match:
+                return colour_code
--- timetable_solutions/tests/unit/domain/view_timetables/test_timetable_colours.py
+++ timetable_solutions/tests/unit/domain/view_timetables/test_timetable_colours.py
@@ -25,7 +25,7 @@ class Test(TestCase):
         pupil = models.Pupil.objects.get_individual_pupil(school_id=123456, pupil_id=1)
         classes = pupil.classes.all()
         expected_colour_dict = {
-            "LUNCH": "#b3b3b3", "BREAK": "#bfbfbf", "FREE": "#feffba",  # Unranked colours
+            "LUNCH": "#b3b3b3", "FREE": "#feffba",  # Generic period colours
             "FRENCH": "#ffbfd6", "MATHS": "#c8d4e3", "ENGLISH": "#b3f2b3",  # Ranked colours
         }
 
@@ -43,7 +43,7 @@ class Test(TestCase):
         teacher = models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=1)
         classes = teacher.classes.all()
         expected_colour_dict = {
-            "LUNCH": "#b3b3b3", "BREAK": "#bfbfbf", "FREE": "#feffba",  # Unranked colours
+            "LUNCH": "#b3b3b3", "FREE": "#feffba",  # Generic period colours
             1: "#c8d4e3", 2: "#b3f2b3",  # Ranked colours
         }
 
@@ -53,12 +53,82 @@ class Test(TestCase):
         # Check outcome
         assert colours_dict == expected_colour_dict
 
-    def test_get_unranked_colours(self):
+    # TESTS FOR HELPER METHODS
+    def test_get_generic_period_colours(self):
         """
-        Unit test for the method returning the unranked colours stored on the TimetableColour Enum
+        Unit test for the method returning the generic period colour dictionary
         """
+        # Set test parameters - in practice classes would be for an individual pupil / teacher, below we use all
+        additional_class = models.FixedClass.create_new(
+            school_id=123456, class_id="Morning break Y1", subject_name="Morning break", user_defined=True,
+            pupils=None, time_slots=None,
+        )
+        all_classes = models.FixedClass.objects.get_all_instances_for_school(school_id=123456)
+
         # Execute test unit
-        unranked_colours = TimetableColour._get_unranked_colours()
+        generic_period_colours = TimetableColour._get_generic_period_colours(classes=all_classes)
 
         # Check outcome
-        assert unranked_colours == {"LUNCH": "#b3b3b3", "BREAK": "#bfbfbf", "FREE": "#feffba"}
+        assert generic_period_colours == {"LUNCH": "#b3b3b3", "MORNING BREAK": "#bfbfbf",  # Morning break gets caps
+                                          TimetableColour.FREE.name: "#feffba"}
+
+    def test_check_class_for_colour_in_regex_successful_meal_time_match_expected(self):
+        """
+        Unit test that any string we expect to match with the regex used to colour code meal times is successful.
+        """
+        # Set test parameters
+        meal_time_strings = [
+            "BREAKFAST", "breakfast", "Breakfast", "Morning breakfast", "Pre-school breakfast",
+            "LUNCH", "lunch", "Lunch", "LuNcH", "   Lunch ", "Lunchtime", "LUNCHTIME", "Luncheon",
+            "TEA", "tea", "Tea",
+            "DINNER", "dinner", "Dinner",
+            "SUPPER", "supper", "Supper"
+        ]
+
+        # Execute test unit and check outcome
+        for meal_time_string in meal_time_strings:
+            colour_code = TimetableColour._check_class_for_colour_in_regex(class_name=meal_time_string)
+            assert colour_code == TimetableColour.MEAL.value
+
+    def test_check_class_for_colour_in_regex_successful_break_match_expected(self):
+        """
+        Unit test that any string we expect to match with the regex used to colour code break times is successful.
+        """
+        # Set test parameters
+        break_time_strings = [
+            "BREAK", "break", "Break", "Morning break", "Afternoon break", "Breaktime", "Year 4 break"
+        ]
+
+        # Execute test unit and check outcome
+        for break_time_strings in break_time_strings:
+            colour_code = TimetableColour._check_class_for_colour_in_regex(class_name=break_time_strings)
+            assert colour_code == TimetableColour.BREAK.value
+
+    def test_check_class_for_colour_in_regex_successful_free_period_match_expected(self):
+        """
+        Unit test that any string we expect to match with the regex used to colour code free periods is successful.
+        """
+        # Set test parameters
+        free_period_strings = [
+            "FREE", "free", "Free", "Free time", "Freetime", "Free period"
+        ]
+
+        # Execute test unit and check outcome
+        for free_time_strings in free_period_strings:
+            colour_code = TimetableColour._check_class_for_colour_in_regex(class_name=free_time_strings)
+            assert colour_code == TimetableColour.FREE.value
+
+    def test_check_class_for_colour_in_regex_unsuccessful_match_against_all_options(self):
+        """
+        Unit test that any string we expect to NOT match with ANY OF THE regexes used to provide default colours to
+        certain subject types returns None from the _check_class_for_colour_in_regex.
+        """
+        # Set test parameters
+        no_match_strings = [
+            "MATHS", "maths", "Maths", "ENGLISH", "english", "English", "FRENCH", "french", "French",  # ETC. etc. Etc.
+        ]
+
+        # Execute test unit and check outcome
+        for no_match_string in no_match_strings:
+            colour_code = TimetableColour._check_class_for_colour_in_regex(class_name=no_match_string)
+            assert colour_code is None
