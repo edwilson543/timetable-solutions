04ff7f6d4bb681201284822694b6ee1c6248d863
edwilson <edward.e.j.wilson@gmail.com>
2022-11-08 10:10:43 +0000

    Added a check to ensure that all classroom_id s are unique for each school, as well as the building/room number combination

--- timetable_solutions/tests/unit/data/models/test_classroom.py
+++ timetable_solutions/tests/unit/data/models/test_classroom.py
@@ -1,5 +1,5 @@
 """
-Unit tests for methods on the Pupil class
+Unit tests for methods on the Classroom class
 """
 
 # Third party imports
@@ -59,7 +59,7 @@ class TestClassroom(test.TestCase):
         is_occupied = classroom.check_if_occupied_at_time_slot(slot=slot)
         assert is_occupied
 
-    def test_check_if_busy_at_time_slot_when_pupil_is_not_busy(self):
+    def test_check_if_busy_at_time_slot_when_classroom_is_not_occupied(self):
         """Test that the check_if_occupied_at_timeslot method returns 'False' when we expect it to"""
         classroom = models.Classroom.objects.get_individual_classroom(school_id=123456, classroom_id=1)
         slot = models.TimetableSlot.objects.get_individual_timeslot(school_id=123456, slot_id=5)
