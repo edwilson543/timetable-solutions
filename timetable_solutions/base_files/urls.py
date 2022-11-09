b4cf2fb05cd399dde0aac092f14c1747939299ce
edwilson <edward.e.j.wilson@gmail.com>
2022-11-09 15:59:51 +0000

    Added a redirect to the base url slug

--- timetable_solutions/base_files/urls.py
+++ timetable_solutions/base_files/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
+
+# Local application imports
+from constants.url_names import UrlName
 
 urlpatterns = [
+    path("", RedirectView.as_view(url=reverse_lazy(UrlName.LOGIN.value))),
     path('api/', include('interfaces.rest_api.urls')),
     path('view/', include('interfaces.view_timetables.urls')),
     path('data/', include('interfaces.data_upload.urls')),
