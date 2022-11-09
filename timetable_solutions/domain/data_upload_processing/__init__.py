f7edef6f5fd24a91dff1ee5124a9b47f17bb4121
edwilson <edward.e.j.wilson@gmail.com>
2022-11-09 09:32:30 +0000

    Defined the class for doing the upload resets

--- timetable_solutions/domain/data_upload_processing/__init__.py
+++ timetable_solutions/domain/data_upload_processing/__init__.py
@@ -4,4 +4,4 @@ Convenience imports. Within the interfaces layer we simply import data_upload_pr
 from .constants import FileStructure, UploadFileStructure
 from .file_upload_processor import FileUploadProcessor
 from .upload_status_tracking import UploadStatusTracker, UploadStatus
-from .reset_uploads import reset_data, UploadsToReset
+from .reset_uploads import UploadsToReset
--- timetable_solutions/domain/data_upload_processing/reset_uploads.py
+++ timetable_solutions/domain/data_upload_processing/reset_uploads.py
@@ -3,19 +3,57 @@ Module defining the logic that allows users to reset their data uploads (i.e. de
 uploaded)
 """
 
-# Standard library imports
-from dataclasses import dataclass
+# Local application imports
+from data import models
 
 
-@dataclass(frozen=True)
 class UploadsToReset:
     """
-    List options for resetting
+    Class to store which tables need resetting, and for which school
     Instantiated by various views
     """
-    Pupil: True
-    pass
+    def __init__(self,
+                 school_access_key: int,
+                 pupils: bool,
+                 teachers: bool,
+                 classrooms: bool,
+                 timetable: bool,
+                 unsolved_classes: bool,
+                 fixed_classes: bool):
+    
+        self._school_access_key = school_access_key
+        
+        # This data gets deleted only if they are passed as True to init
+        self._pupils = pupils
+        self._teachers = teachers
+        self._classrooms = classrooms
+        self._timetable = timetable
+        
+        # Due to protected foreign keys, the fixed / unsolved classes get deleted in any of the following cases
+        self._fixed_classes = teachers or classrooms or fixed_classes
+        self._unsolved_classes = teachers or classrooms or unsolved_classes
 
+        self._reset_data()
 
-def reset_data(tables_to_reset: UploadsToReset):
-    pass
+    def _reset_data(self) -> None:
+        """
+        Function used to reset the school's data as relevant, depending on the dataclass field values.
+        Note that the ORDER in which the models are deleted is very important.
+        """
+        if self._fixed_classes:
+            models.FixedClass.delete_all_user_defined_fixed_classes(school_id=self._school_access_key)
+            
+        if self._unsolved_classes:
+            models.UnsolvedClass.delete_all_instances_for_school(school_id=self._school_access_key)
+            
+        if self._teachers:
+            models.Teacher.delete_all_instances_for_school(school_id=self._school_access_key)
+        
+        if self._classrooms:
+            models.Classroom.delete_all_instances_for_school(school_id=self._school_access_key)
+        
+        if self._pupils:
+            models.Pupil.delete_all_instances_for_school(school_id=self._school_access_key)
+
+        if self._timetable:
+            models.TimetableSlot.delete_all_instances_for_school(school_id=self._school_access_key)
