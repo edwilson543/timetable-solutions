7a98b764c757065bc27b26c44e015d3cce804995
edwilson <edward.e.j.wilson@gmail.com>
2022-11-26 06:22:21 +0000

    Split out the integration tests, the existing file upload processors are working as expected

--- timetable_solutions/tests/integration/domain/data_upload_processing/test_file_upload_processor_integration.py
+++ timetable_solutions/tests/integration/domain/data_upload_processing/test_file_upload_processor_integration.py
@@ -18,6 +18,8 @@ class TestFileUploadProcessorIndependentFilesValidUploads(TestCase):
     """
     Tests for the file uploads that depend on no existing data in the database,
     using files with valid content / structure.
+    These are:
+        - Pupil, Teacher, Classroom, TimetableSlot
     """
 
     fixtures = ["user_school_profile.json"]
@@ -119,86 +121,6 @@ class TestFileUploadProcessorIndependentFilesValidUploads(TestCase):
         self.assertEqual(slot.period_duration, dt.timedelta(hours=1))
 
 
-class TestFileUploadProcessorDependentFilesValidUploads(TestCase):
-    """
-    Tests for the file uploads that depend on existing data in the database, hence requiring fixtures.
-    All tests in this class use files with valid content / structure.
-    """
-
-    fixtures = ["user_school_profile.json", "classrooms.json", "pupils.json", "teachers.json", "timetable.json"]
-    valid_uploads = TEST_DATA_DIR / "valid_uploads"
-
-    def test_upload_unsolved_classes_to_database_valid_upload(self):
-        """
-        Test that the FileUploadProcessor can upload the class requirements (unsolved classes) csv file and use it
-        to populate database
-        """
-        # Set test parameters
-        with open(self.valid_uploads / "class_requirements.csv", "rb") as csv_file:
-            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
-
-        # Upload the file
-        upload_processor = data_upload_processing.FileUploadProcessor(
-            csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.UNSOLVED_CLASSES.headers,
-            id_column_name=data_upload_processing.UploadFileStructure.UNSOLVED_CLASSES.id_column,
-            model=models.UnsolvedClass, school_access_key=123456, is_unsolved_class_upload=True)
-
-        # Test the upload was successful
-        self.assertTrue(upload_processor.upload_successful)
-        self.assertEqual(upload_processor.n_model_instances_created, 12)
-
-        # Test the database is as expected
-        all_classes = models.UnsolvedClass.objects.get_all_instances_for_school(school_id=123456)
-        self.assertEqual(len(all_classes), 12)
-        classes_with_teachers = sum([1 for klass in all_classes if klass.teacher is not None])
-        self.assertEqual(classes_with_teachers, 12)
-        total_non_unique_pupils = sum([klass.pupils.count() for klass in all_classes])
-        self.assertEqual(total_non_unique_pupils, 18)
-
-        # Spot check on one specific UnsolvedClass
-        klass = models.UnsolvedClass.objects.get_individual_unsolved_class(school_id=123456,
-                                                                           class_id="YEAR_ONE_MATHS_A")
-        self.assertQuerysetEqual(klass.pupils.all(), models.Pupil.objects.filter(pupil_id__in={1, 2}), ordered=False)
-        self.assertEqual(klass.teacher, models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=1))
-
-    def test_upload_fixed_classes_to_database_valid_upload(self):
-        """
-        Test that the FileUploadProcessor can upload the fixed classes csv file and use it to populate database
-        """
-        # Set test parameters
-        with open(self.valid_uploads / "fixed_classes.csv", "rb") as csv_file:
-            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
-
-        # Upload the file
-        upload_processor = data_upload_processing.FileUploadProcessor(
-            csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.FIXED_CLASSES.headers,
-            id_column_name=data_upload_processing.UploadFileStructure.FIXED_CLASSES.id_column,
-            model=models.FixedClass, school_access_key=123456, is_fixed_class_upload=True)
-
-        # Test the upload was successful
-        self.assertTrue(upload_processor.upload_successful)
-        self.assertEqual(upload_processor.n_model_instances_created, 12)
-
-        # Test the database is as expected
-        all_classes = models.FixedClass.objects.get_all_instances_for_school(school_id=123456)
-        self.assertEqual(len(all_classes), 12)
-        classes_with_teachers = sum([1 for klass in all_classes if klass.teacher is not None])
-        self.assertEqual(classes_with_teachers, 11)
-        total_non_unique_pupils = sum([klass.pupils.count() for klass in all_classes])
-        self.assertEqual(total_non_unique_pupils, 6)
-        total_non_unique_slots = sum([klass.time_slots.count() for klass in all_classes])
-        self.assertEqual(total_non_unique_slots, 60)
-
-        # Spot checks on a couple of specific FixedClasses
-        pup_lunch = models.FixedClass.objects.get_individual_fixed_class(school_id=123456, class_id="LUNCH_PUPILS")
-        self.assertQuerysetEqual(pup_lunch.pupils.all(),
-                                 models.Pupil.objects.get_all_instances_for_school(school_id=123456), ordered=False)
-
-        teach_ten_lunch = models.FixedClass.objects.get_individual_fixed_class(school_id=123456, class_id="LUNCH_10")
-        self.assertEqual(teach_ten_lunch.teacher,
-                         models.Teacher.objects.get_individual_teacher(school_id=123456, teacher_id=10))
-
-
 class TestFileUploadProcessorIndependentFilesInvalidPupilUploads(TestCase):
     """
     Tests for pupil file uploads with invalid content / structure.
@@ -272,67 +194,6 @@ class TestFileUploadProcessorIndependentFilesInvalidPupilUploads(TestCase):
 
         # Execute test
         self.run_test_for_pupils_with_error_in_row_n(filename=filename, row_n=6)
-
-
-class TestFileUploadProcessorIndependentFilesInvalidFixedClassUploads(TestCase):
-    """
-    Tests for invalid fixed class uploads with invalid content / structure.
-    """
-
-    fixtures = ["user_school_profile.json", "pupils.json", "teachers.json", "timetable.json", "classrooms.json"]
-    invalid_uploads = TEST_DATA_DIR / "invalid_uploads"
-
-    def run_test_for_fixed_classes_with_error(self, filename: str) -> str:
-        """
-        Utility test that can be run for different files, all with different types of error in row n.
-        Note we always test the atomicity of uploads - we want none or all rows of the uploaded file to be
-        processed into the database.
-        :return the error message produced by the processor
-        """
-        # Set test parameters
-        with open(self.invalid_uploads / filename, "rb") as csv_file:
-            upload_file = SimpleUploadedFile(csv_file.name, csv_file.read())
-
-        # Upload the file
-        upload_processor = data_upload_processing.FileUploadProcessor(
-            csv_file=upload_file, csv_headers=data_upload_processing.UploadFileStructure.FIXED_CLASSES.headers,
-            id_column_name=data_upload_processing.UploadFileStructure.FIXED_CLASSES.id_column,
-            model=models.FixedClass, school_access_key=123456, is_fixed_class_upload=True)
-
-        # Check the outcome
-        all_fixed_classes = models.FixedClass.objects.get_all_instances_for_school(school_id=123456)
-        self.assertEqual(all_fixed_classes.count(), 0)
-        self.assertTrue(not upload_processor.upload_successful)
-
-        return upload_processor.upload_error_message
-
-    def test_upload_fixed_classes_file_which_references_a_non_existent_pupil(self):
-        """
-        Unit test that a fixed classes file which references a non-existent pupil is rejected
-        """
-        # Set test parameters
-        filename = "fixed_classes_non_existent_pupil.csv"
-
-        # Execute test
-        error_message = self.run_test_for_fixed_classes_with_error(filename=filename)
-
-        # Check outcome
-        self.assertIn("No pupil", error_message)
-        self.assertIn("7", error_message)  # The non-existent pupil
-
-    def test_upload_fixed_classes_file_which_references_a_non_existent_classroom(self):
-        """
-        Unit test that a fixed classes file which references a non-existent classroom is rejected
-        """
-        # Set test parameters
-        filename = "fixed_classes_non_existent_classroom.csv"
-
-        # Execute test
-        error_message = self.run_test_for_fixed_classes_with_error(filename=filename)
-
-        # Check outcome
-        self.assertIn("Row 2", error_message)
-        self.assertIn("does not exist", error_message)
 
 
 class TestFileUploadProcessorInvalidMiscellaneous(TestCase):
