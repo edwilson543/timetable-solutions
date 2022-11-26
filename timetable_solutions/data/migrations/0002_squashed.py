41c15d44629763eaf7dc57fe6c034f8fb57110e4
edwilson <edward.e.j.wilson@gmail.com>
2022-11-26 08:56:10 +0000

    Deleted the squashed migrations, and updated squashed migration to not be a squashie

--- timetable_solutions/data/migrations/0002_squashed.py
+++ timetable_solutions/data/migrations/0002_squashed.py
@@ -8,8 +8,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('data', '0001_squashed'), ('data', '0003_lesson')]
-
     initial = True
 
     dependencies = [
