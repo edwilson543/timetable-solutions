2f14fd18734b46bce7abcb761c528edaade83a6c
edwilson <edward.e.j.wilson@gmail.com>
2022-11-26 06:18:50 +0000

    Initially split out the file processor into the processing of pupil /teacher etc., and a separate processor fro the leson model

--- timetable_solutions/domain/data_upload_processing/__init__.py
+++ timetable_solutions/domain/data_upload_processing/__init__.py
@@ -3,5 +3,6 @@ Convenience imports. Within the interfaces layer we simply import data_upload_pr
 """
 from .constants import FileStructure, UploadFileStructure
 from .file_upload_processor import FileUploadProcessor
+from .lesson_file_upload_processor import LessonFileUploadProcessor
 from .upload_status_tracking import UploadStatusTracker, UploadStatus
 from .reset_uploads import ResetUploads
