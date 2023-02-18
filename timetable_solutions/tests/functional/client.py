b6b06bdb0712bcbf28e1db56d1ffa7f476ff6859
edwilson <edward.e.j.wilson@gmail.com>
2023-02-18 19:30:08 +0000

    Add webtest to the project requirements

---
+++ timetable_solutions/tests/functional/client.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import models as auth_models
+import django_webtest
+import pytest
+
+from data import models
+from tests import data_factories
+
+
+@pytest.mark.django_db
+class TestClient:
+    """Test client to be used for all functional tests."""
+
+    client: django_webtest.DjangoTestApp
+    """
+    Entrypoint for test functions to the wsgi application.
+    HTTP requests are submitted as e.g. self.client.get(url).
+    """
+
+    @pytest.fixture(autouse=True)
+    def _(self, django_app: django_webtest.DjangoTestApp) -> None:
+        """
+        Use the django_webtest pytest plugin to provide the test wsgi application,
+        and take care of patching and unpatching settings.
+        """
+        self.client = django_app
+
+    def authorise_client_for_school(self, school: models.School) -> None:
+        """Helper method to authorise the test client as a user at the school."""
+        user = auth_models.User.objects.create_user(
+            username="testing", password="unhashed"
+        )
+        data_factories.Profile(user=user, school=school)
+        self.client.set_user(user=user)
+
+    def anonymize_user(self) -> None:
+        """Ensure there is no user authorised on the test client."""
+        self.client.set_user(user=None)
