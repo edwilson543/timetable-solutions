8e96a7585721ceed34bc5916c2ef5d6f64fdd272
edwilson <edward.e.j.wilson@gmail.com>
2023-02-19 15:12:08 +0000

    Add util type for authenticated users, like django-stubs provides

--- timetable_solutions/interfaces/utils/typing_utils.py
+++ timetable_solutions/interfaces/utils/typing_utils.py
@@ -2,16 +2,30 @@
 Utility types for use throughout interfaces layer.
 """
 
-# Standard library imports
-from typing import TypeVar
+from typing import Protocol, TypeVar
 
-# Django imports
 from django import forms
 from django import http
+from django.contrib.auth import models as auth_models
+
+from data import models
 
 
-# Type hint for a request submitted via a htmx ajax request
+class _UserWithProfile(Protocol):
+    """Helper to let type checkers know that users have profiles."""
+
+    profile: models.Profile
+
+
+class AuthenticatedHttpRequest(http.HttpRequest):
+    """Type hint for a HTTP request from an authenticated user."""
+
+    user: auth_models.User | _UserWithProfile
+
+
 class HtmxHttpRequest(http.HttpRequest):
+    """Type hint for a request submitted via a htmx ajax request."""
+
     pass
 
 
