8dbea3a56ccb8a1d61e49967c2c900a871da9d04
edwilson <edward.e.j.wilson@gmail.com>
2023-03-06 08:21:39 +0000

    Add query for getting a year group specific to a school from an id

--- timetable_solutions/domain/data_management/year_groups/queries.py
+++ timetable_solutions/domain/data_management/year_groups/queries.py
@@ -7,6 +7,18 @@ from django.db import models as django_models
 from data import models
 
 
+def get_year_group_for_school(
+    school_id: int, year_group_id: int
+) -> models.YearGroup | None:
+    """Get the year group with the year group id, at the given school."""
+    try:
+        return models.YearGroup.objects.get(
+            year_group_id=year_group_id, school_id=school_id
+        )
+    except models.YearGroup.DoesNotExist:
+        return None
+
+
 def get_next_year_group_id_for_school(school_id: int) -> int:
     """Get the lowest, unused teacher id for a given school."""
     if year_groups := models.YearGroup.objects.filter(school_id=school_id):
--- timetable_solutions/tests/unit/domain/data_management/year_groups/test_queries.py
+++ timetable_solutions/tests/unit/domain/data_management/year_groups/test_queries.py
@@ -9,6 +9,27 @@ from tests import data_factories
 
 
 @pytest.mark.django_db
+class TestGetYearGroupForSchool:
+    def test_gets_year_group(self):
+        yg = data_factories.YearGroup()
+
+        got_yg = queries.get_year_group_for_school(
+            school_id=yg.school.school_access_key, year_group_id=yg.year_group_id
+        )
+
+        assert got_yg == yg
+
+    def test_gets_none_when_year_group_doesnt_exist(self):
+        school = data_factories.School()
+
+        yg = queries.get_year_group_for_school(
+            school_id=school.school_access_key, year_group_id=1
+        )
+
+        assert yg is None
+
+
+@pytest.mark.django_db
 class TestGetNextIdForSchool:
     def test_gets_next_year_group_id_when_school_has_year_groups(self):
         school = data_factories.School()
