44f1d9d4a5d7206b4385bd270b88164e3afb4fb9
edwilson <edward.e.j.wilson@gmail.com>
2023-03-13 08:11:16 +0000

    Add query for searching for pupils by name id or year group

--- timetable_solutions/domain/data_management/pupils/queries.py
+++ timetable_solutions/domain/data_management/pupils/queries.py
@@ -1,4 +1,6 @@
-"""Queries related to managing pupil data."""
+"""
+Queries related to managing pupil data.
+"""
 
 # Django imports
 from django.db import models as django_models
@@ -7,8 +9,33 @@ from django.db import models as django_models
 from data import models
 
 
+def get_pupils(
+    *,
+    school_id: int,
+    search_term: str | None = None,
+    year_group: models.YearGroup | None = None,
+) -> models.PupilQuerySet:
+    """
+    Get the pupils matching some search criteria.
+    """
+    query = django_models.Q(school_id=school_id)
+    if search_term:
+        try:
+            pupil_id = int(search_term)
+            query &= django_models.Q(pupil_id=pupil_id)
+        except ValueError:
+            query &= django_models.Q(
+                firstname__icontains=search_term
+            ) | django_models.Q(surname__icontains=search_term)
+    if year_group:
+        query &= django_models.Q(year_group=year_group)
+    return models.Pupil.objects.filter(query)
+
+
 def get_next_pupil_id_for_school(school_id: int) -> int:
-    """Get the lowest, unused pupil id for a given school."""
+    """
+    Get the lowest, unused pupil id for a given school.
+    """
     if pupils := models.Pupil.objects.filter(school_id=school_id):
         return pupils.aggregate(django_models.Max("pupil_id"))["pupil_id__max"] + 1
     return 1
--- timetable_solutions/tests/unit/domain/data_management/pupils/test_queries.py
+++ timetable_solutions/tests/unit/domain/data_management/pupils/test_queries.py
@@ -11,6 +11,43 @@ from tests import data_factories
 
 
 @pytest.mark.django_db
+class TestGetPupils:
+    @pytest.mark.parametrize("search_name", ["John", "john"])
+    @pytest.mark.parametrize("name_type", ["firstname", "surname"])
+    def test_gets_pupil_matching_name(self, search_name: str, name_type: str):
+        pupil = data_factories.Pupil(**{name_type: "John"})
+        data_factories.Pupil(school=pupil.school, firstname="Dave", surname="Dave")
+
+        pupils = queries.get_pupils(
+            school_id=pupil.school.school_access_key, search_term=search_name
+        )
+
+        assert pupils.get() == pupil
+
+    def test_gets_pupil_matching_year_group(self):
+        pupil = data_factories.Pupil(firstname="John", surname="John")
+        data_factories.Pupil(school=pupil.school, firstname="John", surname="John")
+
+        pupils = queries.get_pupils(
+            school_id=pupil.school.school_access_key, year_group=pupil.year_group
+        )
+
+        assert pupils.get() == pupil
+
+    def test_gets_pupil_matching_pupil_id(self):
+        pupil = data_factories.Pupil()
+        data_factories.Pupil(
+            school=pupil.school,
+        )
+
+        pupils = queries.get_pupils(
+            school_id=pupil.school.school_access_key, search_term=str(pupil.pupil_id)
+        )
+
+        assert pupils.get() == pupil
+
+
+@pytest.mark.django_db
 class TestGetNextIdForSchool:
     def test_gets_next_pupil_id_when_school_has_pupils(self):
         school = data_factories.School()
