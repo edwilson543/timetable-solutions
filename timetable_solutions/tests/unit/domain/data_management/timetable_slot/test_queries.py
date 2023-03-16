80e183d3fda70f890249935581e266f9542f184e
edwilson <edward.e.j.wilson@gmail.com>
2023-03-16 07:18:12 +0000

    Add query to search for timetable slots with

--- timetable_solutions/tests/unit/domain/data_management/timetable_slot/test_queries.py
+++ timetable_solutions/tests/unit/domain/data_management/timetable_slot/test_queries.py
@@ -6,8 +6,71 @@ Tests for data management timetable slot queries.
 import pytest
 
 # Local application imports
+from data import constants
 from domain.data_management.timetable_slot import queries
 from tests import data_factories
+
+
+@pytest.mark.django_db
+class TestGetTimetableSlots:
+    def test_gets_timetable_slot_matching_id(self):
+        slot = data_factories.TimetableSlot(slot_id=1)
+
+        # This slot will not match the slot_id
+        data_factories.TimetableSlot(school=slot.school, slot_id=2)
+        # This slot will be for a different school
+        data_factories.TimetableSlot(slot_id=1)
+
+        slots = queries.get_timetable_slots(
+            school_id=slot.school.school_access_key, slot_id=slot.slot_id
+        )
+
+        assert slots.get() == slot
+
+    def test_gets_timetable_slot_matching_day(self):
+        slot = data_factories.TimetableSlot(day_of_week=constants.Day.MONDAY)
+
+        # This slot will not match the day of the week
+        data_factories.TimetableSlot(
+            school=slot.school, day_of_week=constants.Day.TUESDAY
+        )
+        # This slot will be for a different school
+        data_factories.TimetableSlot(day_of_week=constants.Day.MONDAY)
+
+        slots = queries.get_timetable_slots(
+            school_id=slot.school.school_access_key, day=slot.day_of_week
+        )
+
+        assert slots.get() == slot
+
+    def test_gets_timetable_slot_matching_year_group(self):
+        yg = data_factories.YearGroup()
+        slot = data_factories.TimetableSlot(
+            school=yg.school, relevant_year_groups=(yg,)
+        )
+
+        # This slot will not be associated with the relevant year group
+        data_factories.TimetableSlot(school=slot.school)
+
+        slots = queries.get_timetable_slots(
+            school_id=slot.school.school_access_key, year_group=yg
+        )
+
+        assert slots.get() == slot
+
+    def test_gets_timetable_slot_matching_multiple_parameters(self):
+        slot = data_factories.TimetableSlot()
+
+        # The second slot will be for a different school
+        data_factories.TimetableSlot(slot_id=slot.slot_id, day_of_week=slot.day_of_week)
+
+        slots = queries.get_timetable_slots(
+            school_id=slot.school.school_access_key,
+            slot_id=slot.slot_id,
+            day=slot.day_of_week,
+        )
+
+        assert slots.get() == slot
 
 
 @pytest.mark.django_db
