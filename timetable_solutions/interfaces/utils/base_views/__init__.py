cd7f4bf93c4cde85ccdfcffce1d375c682ac0dc5
edwilson <edward.e.j.wilson@gmail.com>
2023-03-25 11:21:32 +0000

    Add a related list view

--- timetable_solutions/interfaces/utils/base_views/__init__.py
+++ timetable_solutions/interfaces/utils/base_views/__init__.py
@@ -6,6 +6,7 @@ from ._create import CreateView
 from ._download import ExampleDownloadBase
 from ._landing_page import LandingView
 from ._list import ListView
+from ._related_list import RelatedListPartialView
 from ._search import SearchView
 from ._update import UpdateView
 from ._upload import UploadView
