d2d65809fdb4c587261cadd5af7a364c7dc8add5
edwilson <edward.e.j.wilson@gmail.com>
2023-03-26 17:24:29 +0100

    fixup! Add an update related list htmx view

--- timetable_solutions/interfaces/utils/base_views/_update_related_list.py
+++ timetable_solutions/interfaces/utils/base_views/_update_related_list.py
@@ -38,7 +38,7 @@ class UpdateRelatedListPartialView(
         except base_exceptions.UnableToUpdateModelInstance as exc:
             form.add_error(field=None, error=exc.human_error_message)
             return super().form_invalid(form=form)
-        return super().form_valid(form=form)
+        return self._get_success_response()
 
     def get_context_data(self, **kwargs: object) -> dict[str, Any]:
         """
@@ -48,3 +48,30 @@ class UpdateRelatedListPartialView(
         # Include and rename the form
         context["add_form"] = kwargs.get("form", None) or self.get_form()
         return context
+
+    def _get_success_response(self) -> http.HttpResponse:
+        """
+        Since this is an HTMX view, following a successful form operation
+        we just re-render the partial with the updated context, rather than
+        redirecting to some new page as in an ordinary form view.
+        """
+        # Update the object list with the added relation
+        self.object_list = super().get_queryset()
+        form = self.form_class(**self.extra_form_kwargs())
+        context = self.get_context_data(form=form)
+        return super().render_to_response(context=context)
+
+    def get_form_kwargs(self) -> dict[str, Any]:
+        """
+        Combine the default and custom form instantiation kwargs.
+        """
+        return super().get_form_kwargs() | self.extra_form_kwargs()
+
+    def extra_form_kwargs(self) -> dict[str, Any]:
+        """
+        Some extra kwargs to use when instantiating a form.
+
+        These are also used in the success response, hence why they're
+        not just added to get_form_kwargs().
+        """
+        return {}
