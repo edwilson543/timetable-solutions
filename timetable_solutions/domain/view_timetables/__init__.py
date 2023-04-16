43e50ba4acabb67a69095cf8ec0c0d1bf2ef4821
edwilson <edward.e.j.wilson@gmail.com>
2023-04-16 14:22:07 +0100

    Delete the weird timetable summary stat class

--- timetable_solutions/domain/view_timetables/__init__.py
+++ timetable_solutions/domain/view_timetables/__init__.py
@@ -1,9 +1,4 @@
 """
 Convenience imports for objects form view_timetables domain component
 """
-from .queries import (
-    get_letter_indexed_teachers,
-    get_pupil_timetable,
-    get_teacher_timetable,
-)
-from .timetable_summary_stats import SummaryStats
+from .queries import get_pupil_timetable, get_teacher_timetable
