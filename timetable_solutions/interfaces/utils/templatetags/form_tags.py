dc7e8cd2fc18a2729a2a5ca5ce89126c1d21202e
edwilson <edward.e.j.wilson@gmail.com>
2023-04-19 07:54:13 +0100

    Delete now disuses form tag

--- timetable_solutions/interfaces/utils/templatetags/form_tags.py
+++ timetable_solutions/interfaces/utils/templatetags/form_tags.py
@@ -1,47 +1,16 @@
 """
-Module defining custom template tags related to form rendering.
+Custom template tags and filters related to form rendering.
 """
 
 # Standard library imports
 import collections
 import datetime as dt
-import re
 from typing import Any
 
 # Django imports
 from django import forms, template
-from django.utils import html
 
 register = template.Library()
-
-
-@register.simple_tag(name="field_div")
-def render_form_field_in_div(bound_field: forms.BoundField) -> html.SafeString:
-    """
-    # TODO -> DELETE!!!!!!!
-    Template tag rendering the individual form field inside div elements.
-
-    The HTML produced is the same as that produced by django's _html_output, which renders the entire form. It doesn't
-    seem there's a snipped which just renders an individual form field.
-    The use case is initially when adding htmx to forms, it's unideal to write out the html for every field
-    manually. So the one's we want to add htmx to get written manually, the rest use this tag.
-
-    :param bound_field - the field of the form being rendered
-    :return the string formatted as a SafeString, using html
-    """
-    text = f"""
-    <div>
-        { bound_field.label_tag() }
-        <div class="helptext">
-            { bound_field.help_text }
-        </div>
-        { bound_field.errors }
-        { bound_field }
-    </div>
-    """
-    text = text.strip()
-    text = re.sub(r"\n\s*\n", "\n", text)
-    return html.format_html(text)
 
 
 @register.filter(name="is_basic_input_widget")
--- timetable_solutions/tests/unit/interfaces/utils/templatetags/test_form_tags.py
+++ timetable_solutions/tests/unit/interfaces/utils/templatetags/test_form_tags.py
@@ -1,56 +1,12 @@
 """
-Unit tests for the custom form tags.
+Unit tests for the custom form tags and filters.
 """
-
 
 # Third party imports
 import pytest
 
 # Django imports
 from django import forms, template
-from django.utils import html
-
-
-@pytest.fixture(scope="module")
-def form() -> type[forms.Form]:
-    """
-    Fixture to provide a dummy form for use in tests.
-    """
-
-    class TestForm(forms.Form):
-        text = forms.CharField()
-
-    return TestForm
-
-
-def test_render_form_field_in_div_renders_text_field_correctly(form: type[forms.Form]):
-    """
-    Test that the correct html is returned from the 'text' field.
-    """
-    # Set test parameters
-    template_string = """
-    {% load form_tags %}
-    {% field_div form.text %}
-    """
-    temp = template.Template(template_string=template_string)
-    context = template.Context({"form": form})
-
-    # Execute test unit
-    outcome = temp.render(context=context)
-
-    # Check outcome
-    expected_html = html.SafeString(
-        """
-    <div>
-        <label for="id_text">Text:</label>
-        <div class="helptext">
-        </div>
-        <input type="text" name="text" required id="id_text">
-    </div>
-    """
-    )
-    assert isinstance(outcome, html.SafeString)
-    assert expected_html in outcome
 
 
 class TestIsBasicInputWidget:
