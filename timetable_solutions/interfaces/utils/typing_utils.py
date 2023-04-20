f88ac6c6209970a501c959bab4422f9fb9a7951a
edwilson <edward.e.j.wilson@gmail.com>
2023-04-20 07:49:52 +0100

    Declare that registered users have a profile

--- timetable_solutions/interfaces/utils/typing_utils.py
+++ timetable_solutions/interfaces/utils/typing_utils.py
@@ -2,28 +2,34 @@
 Utility types for use throughout interfaces layer.
 """
 
-# Standard library imports
-from typing import TypeVar
-
 # Third party imports
 from django_htmx.middleware import HtmxDetails
 
 # Django imports
-from django import forms, http
+from django import http
 from django.contrib.auth import models as auth_models
+
+# Local application imports
+from data import models
+
+
+class RegisteredUser(auth_models.User):
+    """A user who has completed registration and therefore has a profile."""
+
+    profile: models.Profile
+
+    class Meta:
+        # Ensure django doesn't think this is some new model
+        abstract = True
 
 
 class AuthenticatedHttpRequest(http.HttpRequest):
     """Type hint for an HTTP request from an authenticated user."""
 
-    user: auth_models.User
+    user: RegisteredUser
 
 
 class AuthenticatedHtmxRequest(AuthenticatedHttpRequest):
     """Type hint for a request submitted via an htmx ajax request."""
 
     htmx: HtmxDetails
-
-
-# Generic type hint for a form
-FormSubclass = TypeVar("FormSubclass", bound=forms.Form)
